352695b2fd2895e84e543c0500371f8da98669c1
Leonardo Cofré <leocofre1981@gmail.com>
2022-09-30 22:53:09 -0300

    Fibonacci con for, while y do-while

--- src/Practicas/Fibonacci.java
+++ src/Practicas/Fibonacci.java
@@ -4,7 +4,15 @@ public class Fibonacci {
 
     public static void main(String[] args) {
 
+        System.out.println("Secuencia de Fibonacci Con for ");
         fibonacciConFor();
+        System.out.println("");
+        System.out.println("Secuencia de Fibonacci Con While ");
+        fibonacciConWhile();
+        System.out.println("");
+        System.out.println("Secuencia de Fibonacci Con do-While ");
+        fibonacciConDoWhile();
+
     }
 
     public static void fibonacciConFor() {
@@ -20,5 +28,36 @@ public class Fibonacci {
                 System.out.print(a);
             }
         }
+    }
+
+    public static void fibonacciConWhile() {
+        int a = 0, b = 1, c = 0, i = 0;
+        while (i < 10) {
+            if (i < 9) {
+                System.out.print(a + ",");
+                c = a + b;
+                a = b;
+                b = c;
+            } else {
+                System.out.print(a);
+            }
+            i++;
+        }
+    }
+
+    public static void fibonacciConDoWhile() {
+        int a = 0, b = 1, c = 0, i = 0;
+
+        do {
+            if (i < 9) {
+                System.out.print(a + ",");
+                c = a + b;
+                a = b;
+                b = c;
+            } else {
+                System.out.print(a);
+            }
+            i++;
+        } while (i < 10);
     }
 }
