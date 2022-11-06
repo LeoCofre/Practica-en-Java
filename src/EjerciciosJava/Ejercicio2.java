91b12ac05c9f2a05622bb9a6e4b728b90bbd2e98
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-06 00:22:38 -0300

    Ejercicio 2

--- src/EjerciciosJava/Ejercicio2.java
+++ src/EjerciciosJava/Ejercicio2.java
@@ -2,29 +2,29 @@ package EjerciciosJava;
 
 import java.util.Scanner;
 
-public class Ejercicio1 {
-
+public class Ejercicio2 {
     public static void main(String[] args) {
 
-        //Suma de dos numeros
+        //Hallar el cociente y el residuo (resto) de dos números enteros.
 
         //Variables
-        int numero1, numero2, resultado;
+        int numero1, numero2, cociente, residuo;
 
         //Entrada
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Numero 1: ");
+        System.out.println("Numero 1");
         numero1 = teclado.nextInt();
         System.out.println("Numero 2");
         numero2 = teclado.nextInt();
 
         //Proceso
-        resultado = numero1 + numero2;
+        cociente = numero1 / numero2;
+        residuo = numero1 % numero2;
 
         //Salida
-
         System.out.println("");
-        System.out.println("Suma:" + resultado);
+        System.out.println("Cociente: " + cociente);
+        System.out.println("Residuo: "+ residuo);
 
     }
 }
