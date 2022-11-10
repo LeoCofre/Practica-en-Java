5a17b23c3b8c76174422c4a2b93367ef533657b7
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-10 02:01:27 -0300

    Ejercicio 10

--- src/EjerciciosJava/Ejercicio10.java
+++ src/EjerciciosJava/Ejercicio10.java
@@ -1,9 +1,36 @@
 package EjerciciosJava;
 
+import java.util.Scanner;
+
 public class Ejercicio10 {
 
     public static void main(String[] args) {
 
+        //convertir una cantidad de segundos en horas, minutos y segundos.
+
+        //Constantes
+        final int HORA = 3600;
+        final int MINUTO = 60;
+
+        //Variables
+        int tiempo, hora, minuto, segundo;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Tiempo en segundos");
+        tiempo = teclado.nextInt();
+
+        //Proceso
+        hora = tiempo / HORA;
+        tiempo = tiempo % HORA;
+        minuto = tiempo / MINUTO;
+        segundo = tiempo % MINUTO;
+
+        //Salida
+        System.out.println("");
+        System.out.println("Horas: " + hora);
+        System.out.println("Minutos: " + minuto);
+        System.out.println("Segundos: " + segundo);
 
     }
 }
