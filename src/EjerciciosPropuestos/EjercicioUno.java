8474e840acada36620c284f84aff6e8397c5fbd7
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-10 21:10:26 -0300

    Ejercicio propuesto numero uno.

---
+++ src/EjerciciosPropuestos/EjercicioUno.java
@@ -0,0 +1,29 @@
+package EjerciciosPropuestos;
+
+import java.util.Scanner;
+
+public class EjercicioUno {
+    public static void main(String[] args) {
+
+        //Dado dos números enteros (Z), a y b, hallar a + by a- b.
+
+        //Variables
+        int primerNumero, segundoNumero, resultadoSuma, resultadoResta;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese primer número");
+        primerNumero = teclado.nextInt();
+        System.out.println("Ingrese segundo número");
+        segundoNumero = teclado.nextInt();
+
+        //Proceso
+        resultadoSuma = primerNumero + segundoNumero;
+        resultadoResta = primerNumero - segundoNumero;
+
+        //Salida
+        System.out.println("El resultado de la  suma de los números es: " + resultadoSuma);
+        System.out.println("El resultado de La resta de los números es: " + resultadoResta);
+
+    }
+}
