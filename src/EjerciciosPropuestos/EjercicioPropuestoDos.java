9bd3546cf774948ee0d2487dde544066be2b470d
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-10 23:22:03 -0300

    Ejercicio propuesto numero dos.

---
+++ src/EjerciciosPropuestos/EjercicioPropuestoDos.java
@@ -0,0 +1,33 @@
+package EjerciciosPropuestos;
+
+import java.util.Scanner;
+
+public class EjercicioPropuestoDos {
+    public static void main(String[] args) {
+
+        //Dado dos números enteros, determinar cuántos números enteros están incluidos en ellos.
+
+        //Variables
+        int primerNumero, segundoNumero;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingresa un número");
+        primerNumero = teclado.nextInt();
+        segundoNumero = 0;
+
+        //Proceso
+        while (primerNumero >= segundoNumero) {
+            System.out.println("Ingrese otro número");
+            segundoNumero = teclado.nextInt();
+        }
+
+        while (segundoNumero >= primerNumero) {
+            System.out.println(segundoNumero);
+            segundoNumero = segundoNumero - 1;
+        }
+
+        //Salida
+        System.out.println(segundoNumero);
+    }
+}
