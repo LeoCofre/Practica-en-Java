c5a9931da99d89d6b1203acf843c5a5aa76cccb3
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-12 14:34:46 -0300

    Ejercicio propuesto numero tres.

--- src/EjerciciosPropuestos/Ejercicio03.java
+++ src/EjerciciosPropuestos/Ejercicio03.java
@@ -14,12 +14,29 @@ public class Ejercicio03 {
         final float DECIMETROS = 100;
         final float CENTIMETROS = 10;
 
+
         // Variables
         float milimetros, resultadoMetros, resultadoDecimetros, resultadoCentimetros, resultadoMilimetros;
 
         //Entrada
         Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese cantidas de milimetros");
+        milimetros = teclado.nextFloat();
 
+        //Proceso
+        resultadoMetros = milimetros / METROS;
+        milimetros = milimetros % METROS;
+        resultadoDecimetros = milimetros / DECIMETROS;
+        milimetros = milimetros % DECIMETROS;
+        resultadoCentimetros = milimetros / CENTIMETROS;
+        resultadoMilimetros = milimetros % CENTIMETROS;
+
+
+        //Salida
+        System.out.println("Metros: " + resultadoMetros);
+        System.out.println("Decimetros: " + resultadoDecimetros);
+        System.out.println("Centimetros: " + resultadoCentimetros);
+        System.out.println("Milimetros: " + resultadoMilimetros);
 
     }
 }
