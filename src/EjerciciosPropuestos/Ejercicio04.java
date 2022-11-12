8e8f04b16f8b90574b9850b60fe6ee2fea91210c
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-12 19:54:57 -0300

    Ejercicio propuesto numero cuatro.

---
+++ src/EjerciciosPropuestos/Ejercicio04.java
@@ -0,0 +1,31 @@
+package EjerciciosPropuestos;
+
+import java.util.Scanner;
+
+public class Ejercicio04 {
+
+    public static void main(String[] args) {
+
+        //Calculos de formulas matematicas
+
+        //Variables
+        int a, b;
+        double c, d;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese valor de a: ");
+        a = teclado.nextInt();
+        System.out.println("Ingrese valor de b: ");
+        b = teclado.nextInt();
+
+        //Proceso
+        c = ((double) 4 * Math.pow(a, 4) + 3 * b * a + Math.pow(b, 2)) / (Math.pow(a, 2) - Math.pow(b, 2));
+        d = ((double) 4 * Math.pow(c, 2) + a + b) / 4;
+
+        //Salida
+        System.out.println("El valor de c es: " + c);
+        System.out.println("El valor de d es: " + d);
+
+    }
+}
