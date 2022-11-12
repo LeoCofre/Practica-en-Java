86489d3e6dc50061a5caa2efb2e5dc90be573a24
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-11 22:21:30 -0300

    Ejercicio propuesto numero tres.

---
+++ src/EjerciciosPropuestos/Ejercicio03.java
@@ -0,0 +1,20 @@
+package EjerciciosPropuestos;
+
+import java.util.Scanner;
+
+public class Ejercicio03 {
+    public static void main(String[] args) {
+
+        //Dada una cantidad de milímetros, expresarlo en la máxima cantidad de metros, el resto en
+        //decímetros, centímetros y milímetros.
+
+        // Variables
+        float entradaMilimetros, metros, decimetros, centimetros, milimetros;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        entradaMilimetros = teclado.nextFloat();
+
+
+    }
+}
