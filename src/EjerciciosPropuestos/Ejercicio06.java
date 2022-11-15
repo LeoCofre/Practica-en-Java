fe4a5e4776ae65156a27f52d09c2f7b2ea912634
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-14 23:58:48 -0300

    Ejercicio propuesto numero seis.

--- src/EjerciciosPropuestos/Ejercicio06.java
+++ src/EjerciciosPropuestos/Ejercicio06.java
@@ -3,5 +3,11 @@ package EjerciciosPropuestos;
 public class Ejercicio06 {
     public static void main(String[] args) {
 
+        //Hallar el área y el perímetro de un cuadrado.
+
+        //Variables
+
+
+
     }
 }
