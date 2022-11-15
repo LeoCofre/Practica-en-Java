ad7795804431aec7f2e623baffd5c11fb10eff0b
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-15 20:03:25 -0300

    Ejercicio propuesto numero seis.

--- src/EjerciciosPropuestos/Ejercicio06.java
+++ src/EjerciciosPropuestos/Ejercicio06.java
@@ -1,12 +1,30 @@
 package EjerciciosPropuestos;
 
+import java.util.Scanner;
+
 public class Ejercicio06 {
     public static void main(String[] args) {
 
         //Hallar el área y el perímetro de un cuadrado.
 
-        //Variables
+        //Constantes
+        final int PERIMETRO;
+        final double AREA;
 
+        //Variables
+        int lado;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese Numero de un lado: ");
+        lado = teclado.nextInt();
+
+        //Proceso
+        PERIMETRO = lado * 4;
+        AREA = Math.pow(lado, 2);
+
+        //Salida
+        System.out.println("El perimetro del cuadrado con un lado de " + lado + " es: " + PERIMETRO + "y el area es: " + AREA);
 
 
     }
