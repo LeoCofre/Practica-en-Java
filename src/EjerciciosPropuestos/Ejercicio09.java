962d074738eea1cbb402d6cfa77276f56c804c71
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-15 23:44:36 -0300

    Ejercicio propuesto numero nueve.

--- src/EjerciciosPropuestos/Ejercicio09.java
+++ src/EjerciciosPropuestos/Ejercicio09.java
@@ -15,14 +15,16 @@ public class Ejercicio09 {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Ingrese valor de lado 1: ");
         ladoUno = teclado.nextInt();
-        System.out.println("Ingrese valos de ldo 2");
+        System.out.println("Ingrese valos de lado 2");
         ladoDos = teclado.nextInt();
 
         //Proceso
-        area =  ladoUno * ladoDos;
+        area = ladoUno * ladoDos;
         perimetro = 2 * (ladoUno + ladoDos);
 
         //Salida
-        System.out.println("");
+        System.out.println("El area para los lados " + ladoUno + " y " + ladoDos + " es " + area + " y el perimetro es "
+                + perimetro);
+
     }
 }
