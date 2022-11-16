9393153228c7a9088dc28796bd97fc17487bd8b7
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-15 21:16:52 -0300

    Ejercicio propuesto numero ocho.

---
+++ src/EjerciciosPropuestos/Ejercicio08.java
@@ -0,0 +1,22 @@
+package EjerciciosPropuestos;
+
+import java.util.Scanner;
+
+public class Ejercicio08 {
+
+    public static void main(String[] args) {
+
+        //Convertir una cantidad de grados Fahrenheit a Celsius y Kelvin.
+
+        //Constantes
+        final int FAHRENHEIT;
+        final float celsius, kelvin;
+
+        //Variables
+        float resultadoCelsius, resultadokelvin;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+
+    }
+}
