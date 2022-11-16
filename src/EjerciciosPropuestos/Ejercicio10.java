aaf61d4cfd0c4b48a78b99a3288f09ecd62ff3ac
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-16 00:01:28 -0300

    Ejercicio propuesto numero nueve.

---
+++ src/EjerciciosPropuestos/Ejercicio10.java
@@ -0,0 +1,26 @@
+package EjerciciosPropuestos;
+
+import java.util.Scanner;
+
+public class Ejercicio10 {
+
+    public static void main(String[] args) {
+
+        //Convertir grados sexagesimales a centesimales.
+
+        //Variables
+        double sexagesimal, centesimal;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese numero sexagesimal. ");
+        sexagesimal = teclado.nextInt();
+
+        //Proceso
+        centesimal = ((200 * sexagesimal) / 180);
+
+        //Salida
+        System.out.println("Los grados centesimales para " + sexagesimal + " es " + centesimal);
+
+    }
+}
