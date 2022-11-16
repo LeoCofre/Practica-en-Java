47ecc50645a9585a1a9cc3e2ccae0add9cabaf10
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-15 21:03:20 -0300

    Ejercicio propuesto numero siete.

--- src/EjerciciosPropuestos/Ejercicio06.java
+++ src/EjerciciosPropuestos/Ejercicio06.java
@@ -3,6 +3,7 @@ package EjerciciosPropuestos;
 import java.util.Scanner;
 
 public class Ejercicio06 {
+
     public static void main(String[] args) {
 
         //Hallar el área y el perímetro de un cuadrado.
---
+++ src/EjerciciosPropuestos/Ejercicio07.java
@@ -0,0 +1,33 @@
+package EjerciciosPropuestos;
+
+import java.util.Scanner;
+
+public class Ejercicio07 {
+
+    public static void main(String[] args) {
+
+        //Dada una cantidad de horas, obtener su equivalente en minutos y segundos.
+
+
+        //Variables
+        int minuto = 60;
+        int segundo = 360;
+        int horas, resultadoMinuto, resultadoSegundo;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese cantidad de horas a calcular: ");
+        horas = teclado.nextInt();
+
+        //Proceso
+        resultadoMinuto = horas * minuto;
+        resultadoSegundo = (horas % minuto) - horas;
+       /* horas = resultadoSegundo * segundo;
+        horas = resultadoSegundo % segundo;*/
+
+        //Salida
+        System.out.println("La cantidad de horas ingresadas equivale a: " + resultadoMinuto + "minutos y "
+                + resultadoSegundo + "segundos");
+
+    }
+}
