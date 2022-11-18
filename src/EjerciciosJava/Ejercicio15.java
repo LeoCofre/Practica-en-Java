de650a2c65efd30408b68dffdf772779fdd8fe80
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-17 22:56:27 -0300

    Ejercicio numero quince.

---
+++ src/EjerciciosJava/Ejercicio15.java
@@ -0,0 +1,31 @@
+package EjerciciosJava;
+
+import java.util.Scanner;
+
+public class Ejercicio15 {
+
+    public static void main(String[] args) {
+
+        //Determinar si un número entero es par o impar.
+
+        //Variables
+        int numero;
+        String respuesta = "";
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese numero: ");
+        numero = scanner.nextInt();
+
+        //Proceso
+        if (numero % 2 == 0) {
+            respuesta = " es par";
+        } else {
+            respuesta = " es impar";
+        }
+
+        //Salida
+        System.out.println("El numero ingresado "  + respuesta);
+
+    }
+}
