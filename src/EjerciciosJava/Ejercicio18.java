12de026cb0200b0ae78d14a45ec2b9d69f990c20
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-18 20:45:12 -0300

    Ejercicio numero diecisiete.

---
+++ src/EjerciciosJava/Ejercicio18.java
@@ -0,0 +1,53 @@
+package EjerciciosJava;
+
+import java.util.Scanner;
+
+public class Ejercicio18 {
+
+    public static void main(String[] args) {
+
+        //Dados 3 números, devolver los números en orden ascendente.
+
+        //Variables
+        int primerNumero, segundoNumero, tercerNumero, numeroMayor, numeroIntermedio, numeroMenor;
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese primer número: ");
+        primerNumero = scanner.nextInt();
+        System.out.println("Ingrese segundo número: ");
+        segundoNumero = scanner.nextInt();
+        System.out.println("Ingrese tercer número: ");
+        tercerNumero = scanner.nextInt();
+
+        //Proceso
+        if (primerNumero > segundoNumero && primerNumero > tercerNumero) {
+            numeroMayor = primerNumero;
+        } else {
+            if (segundoNumero > primerNumero && segundoNumero > tercerNumero) {
+                numeroMayor = segundoNumero;
+            } else
+                numeroMayor = tercerNumero;
+        }
+
+        if (primerNumero < segundoNumero && primerNumero < tercerNumero) {
+            numeroMenor = primerNumero;
+        } else {
+            if (segundoNumero < primerNumero && segundoNumero < tercerNumero) {
+
+                numeroMenor = segundoNumero;
+            } else {
+                numeroMenor = tercerNumero;
+            }
+        }
+        numeroIntermedio = (primerNumero + segundoNumero + tercerNumero) - (numeroMayor + numeroMenor);
+
+        // Salida
+        System.out.println("");
+        System.out.println("Mayor : " + numeroMayor);
+        System.out.println("Intermedio : " + numeroIntermedio);
+        System.out.println("Menor : " + numeroMenor);
+    }
+
+}
+
