c95b1409e5a1ad0fed2151bb0e526a14f25428b3
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-18 20:50:26 -0300

    Ejercicio numero diecinueve.

---
+++ src/EjerciciosJava/Ejercicio19.java
@@ -0,0 +1,14 @@
+package EjerciciosJava;
+
+public class Ejercicio19 {
+
+    public static void main(String[] args) {
+
+        /*Un restaurante ofrece un descuento del 10 % para consumos de hasta S/.100.00 y un
+        descuento de 20% para consumos mayores. Para ambos casos se aplica un impuesto del 19 %. Determinar
+        el monto del descuento, el impuesto y el importe a pagar.*/
+
+        //Variables
+
+    }
+}
