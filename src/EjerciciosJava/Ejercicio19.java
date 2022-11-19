d2e0fe3e54be5dccb13fc9c5cc9f06d0e6e2fa56
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-18 21:09:58 -0300

    Ejercicio numero diecinueve.

--- src/EjerciciosJava/Ejercicio19.java
+++ src/EjerciciosJava/Ejercicio19.java
@@ -1,5 +1,7 @@
 package EjerciciosJava;
 
+import java.util.Scanner;
+
 public class Ejercicio19 {
 
     public static void main(String[] args) {
@@ -8,7 +10,34 @@ public class Ejercicio19 {
         descuento de 20% para consumos mayores. Para ambos casos se aplica un impuesto del 19 %. Determinar
         el monto del descuento, el impuesto y el importe a pagar.*/
 
+        //Constantes
+        final float DESCUENTO_DIEZ_PORCIENTO = 00.1F;
+        final float DESCUENTO_VEINTE_PORCIENTO = 0.2F;
+        final float IVA = 0.19F;
+
         //Variables
+        float consumo, montoDescuento, montoIva, total;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Consumo : ");
+        consumo = teclado.nextFloat();
+
+        //Proceso
+        if (consumo <= 100) {
+            montoDescuento = consumo * DESCUENTO_DIEZ_PORCIENTO;
+        } else {
+            montoDescuento = consumo * DESCUENTO_VEINTE_PORCIENTO;
+        }
+
+        montoIva = (consumo - montoDescuento) * IVA;
+        total = consumo - montoDescuento + montoIva;
+
+        //Salida
+        System.out.println(" ");
+        System.out.println("Monto descuento : " + montoDescuento);
+        System.out.println(" Impuesto IGV : " + montoIva);
+        System.out.println("Importe a pagar : " + total);
 
     }
 }
