cac72488ad5bfc5b36776b1b697a392af7f4bad6
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-19 23:06:09 -0300

    Ejercicio numero veintiuno.

---
+++ src/EjerciciosJava/Ejercicio21.java
@@ -0,0 +1,17 @@
+package EjerciciosJava;
+
+public class Ejercicio21 {
+
+    public static void main(String[] args) {
+
+        //Al ingresar el valor de una temperatura, obtener el tipo de clima según la siguiente tabla.
+
+        /*Temperatura Tipo de clima
+        Temp. < 10 Frío
+        Temp. Entre 10 Y 20 Nublado
+        Temp. Entre 21 Y 30 Calor
+        Temp. > 30 Tropi cal*/
+
+
+    }
+}
