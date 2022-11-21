39131136185b529025c41437e4decb0e583f9052
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-20 23:49:26 -0300

    Ejercicio numero veintidos.

--- src/EjerciciosJava/Ejercicio22.java
+++ src/EjerciciosJava/Ejercicio22.java
@@ -4,6 +4,14 @@ public class Ejercicio22 {
 
     public static void main(String[] args) {
 
-        //
+        /*Un negocio tiene dos tipos de cliente: cliente general (G) y cliente afiliado (A). Acepta dos
+        formas de pago: al contado (C) y en plazos (P). Nos piden crear un programa que al ingresar el monto de
+        la compra se obtenga el monto del descuento o el monto del recargo y el total a pagar, según la siguiente
+        tabla. */
+
+        /*Tipo______________Contado (C)___________ Plazos (P)
+                            descuento              recargo
+        Cliente general (G)     15 %                10%
+        Cliente afiliado (A)    20%                 5%*/
     }
 }
