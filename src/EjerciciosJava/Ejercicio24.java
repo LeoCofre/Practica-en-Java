da84def593e134e2d09f0ad3915a5ab7c5d398cb
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-23 23:25:16 -0300

    Ejercicio numero veinticuatro.

--- src/EjerciciosJava/Ejercicio24.java
+++ src/EjerciciosJava/Ejercicio24.java
@@ -1,9 +1,37 @@
 package EjerciciosJava;
 
+import java.util.Scanner;
+
 public class Ejercicio24 {
 
     public static void main(String[] args) {
 
+        //Variables
+        double a, b, e, d, x1, x2;
 
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("a : ");
+        a = teclado.nextDouble();
+        System.out.print("b : ");
+        b = teclado.nextDouble();
+        System.out.print("c : ");
+        e = teclado.nextDouble();
+
+        //Proceso
+        d = Math.pow(b, 2.0) - 4.0 * a * e;
+
+        if (a != 0 && d >= 0) {
+            x1 = (-b + Math.pow(d, (1.0 / 2.0))) / 2 * a;
+            x2 = (-b - Math.pow(d, (1.0 / 2.0))) / 2 * a;
+        } else {
+            x1 = 0;
+            x2 = 0;
+
+        }
+
+        //Salida
+        System.out.println("");
+        System.out.println("x1: "  );
     }
 }
