bb9aef8d619cc702f5760cf1d5d68b58ffa8c02b
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-25 01:20:47 -0300

    Ejercicio propuesto numero once.

---
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio12.java
@@ -0,0 +1,31 @@
+package EjerciciosPropuestos.OnceAlVeinte;
+
+import java.util.Scanner;
+
+public class Ejercicio12 {
+
+    public static void main(String[] args) {
+
+        //Dados dos números enteros, devolver el número menor.
+
+        //Variables
+        int primerNumero, segundoNumero, numeroMenor;
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ingrese un número: ");
+        primerNumero = scanner.nextInt();
+        System.out.println("Ingrese otro número: ");
+        segundoNumero = scanner.nextInt();
+
+        //Proceso
+        if (primerNumero > segundoNumero) {
+            numeroMenor = segundoNumero;
+        } else {
+            numeroMenor = primerNumero;
+        }
+
+        //Salida
+        System.out.println("El número menor es: " + numeroMenor);
+    }
+}
