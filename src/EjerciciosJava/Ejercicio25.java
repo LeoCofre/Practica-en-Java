6de1f53590cc7ea1f6b7c5f72da6c5ec5ca13a14
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-24 23:03:28 -0300

    Ejercicio numero veinticinco.

--- src/EjerciciosJava/Ejercicio25.java
+++ src/EjerciciosJava/Ejercicio25.java
@@ -1,8 +1,22 @@
 package EjerciciosJava;
 
+import java.util.Scanner;
+
 public class Ejercicio25 {
 
     public static void main(String[] args) {
+
+        //Dado la hora, minuto y segundo, encuentre la hora del siguiente segundo.
+
+        //Variables
+        int hora, minuto, segundo;
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese hora: ");
+        hora = scanner.nextInt();
+        System.out.println("Ingrese minuto: ");
+
 
 
     }
