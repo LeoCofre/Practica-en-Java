ec06aefc457d82f32927289405458ac444b62753
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-25 00:41:39 -0300

    Ejercicio propuesto numero once.

--- src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio11.java
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio11.java
@@ -1,5 +1,7 @@
 package EjerciciosPropuestos.OnceAlVeinte;
 
+import java.util.Scanner;
+
 public class Ejercicio11 {
 
     public static void main(String[] args) {
@@ -12,12 +14,19 @@ public class Ejercicio11 {
         String respuesta = "";
 
         //Entrada
-        System.out.println();
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese edad: ");
+        edad = scanner.nextInt();
 
         //Proceso
+        if (edad >= 18){
+            respuesta = "Eres mayor de edad";
+        }else{
+            respuesta = "Eres menor de edad";
+        }
 
-
+        //Salida
+        System.out.println(respuesta);
 
 
     }
