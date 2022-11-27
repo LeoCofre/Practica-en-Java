3a376b196852278d8613128e7a4f1a3185e35c4f
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-26 23:11:28 -0300

    Ejercicio propuesto numero trece.

--- src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio13.java
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio13.java
@@ -1,10 +1,34 @@
 package EjerciciosPropuestos.OnceAlVeinte;
 
+import java.util.Scanner;
+
 public class Ejercicio13 {
 
     public static void main(String[] args) {
 
         //Dados dos números enteros, devolver el número menor.
+
+        //Variables
+        int primerNumero, segundo_numero, numeroMenor;
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese primer número: ");
+        primerNumero = scanner.nextInt();
+        System.out.println("Ingrese segundo número: ");
+        segundo_numero = scanner.nextInt();
+
+        //Proceso
+        if (primerNumero < segundo_numero){
+            numeroMenor = primerNumero;
+        }else {
+            numeroMenor = segundo_numero;
+        }
+
+        //Salida
+        System.out.println("El número menor es: " + numeroMenor);
+
+
 
     }
 }
