67f2ab7f204fdb96c2e136a95de94bd0869e3d0f
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-26 23:14:49 -0300

    Ejercicio propuesto numero catorce.

---
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio14.java
@@ -0,0 +1,12 @@
+package EjerciciosPropuestos.OnceAlVeinte;
+
+public class Ejercicio14 {
+
+    public static void main(String[] args) {
+
+        //Dados dos números, determinar si son iguales o son diferentes.
+
+        //Variables
+        int primerNumero, segundoNumero;
+    }
+}
