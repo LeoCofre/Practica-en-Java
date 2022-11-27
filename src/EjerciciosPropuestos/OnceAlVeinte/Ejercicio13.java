0a000a90f4ae4baafe20ccdca9be7b0866e48b17
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-26 23:04:12 -0300

    Ejercicio propuesto numero trece.

---
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio13.java
@@ -0,0 +1,10 @@
+package EjerciciosPropuestos.OnceAlVeinte;
+
+public class Ejercicio13 {
+
+    public static void main(String[] args) {
+
+        //Dados dos números enteros, devolver el número menor.
+
+    }
+}
