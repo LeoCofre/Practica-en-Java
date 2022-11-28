be23921c6d72c5798aa463f7e600473a08739115
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-27 23:26:22 -0300

    Ejercicio propuesto numero quince.

---
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio15.java
@@ -0,0 +1,10 @@
+package EjerciciosPropuestos.OnceAlVeinte;
+
+public class Ejercicio15 {
+
+    public static void main(String[] args) {
+
+       /*Dado un número entero, devolver el doble del número si el número es positivo; el triple del
+        número si es negativo; y cero si el número es neutro.*/
+    }
+}
