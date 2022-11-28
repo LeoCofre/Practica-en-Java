edcb7322542ca591c1c1979d8d5459bdf8a7f969
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-27 23:32:23 -0300

    Ejercicio propuesto numero dieciseis.

--- src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio13.java
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio13.java
@@ -6,27 +6,14 @@ public class Ejercicio13 {
 
     public static void main(String[] args) {
 
-        //Dados dos números enteros, devolver el número menor.
+        // Dados dos números, determinar si son iguales o son diferentes.
 
         //Variables
-        int primerNumero, segundo_numero, numeroMenor;
 
-        //Entrada
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese primer número: ");
-        primerNumero = scanner.nextInt();
-        System.out.println("Ingrese segundo número: ");
-        segundo_numero = scanner.nextInt();
 
         //Proceso
-        if (primerNumero < segundo_numero){
-            numeroMenor = primerNumero;
-        }else {
-            numeroMenor = segundo_numero;
-        }
 
         //Salida
-        System.out.println("El número menor es: " + numeroMenor);
 
 
 
