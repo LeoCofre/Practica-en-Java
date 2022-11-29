5c37b17f5be7cca076f39f92960813dace551aa5
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-28 22:12:23 -0300

    Ejercicio propuesto numero trece.

--- src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio13.java
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio13.java
@@ -10,12 +10,24 @@ public class Ejercicio13 {
 
         //Variables
         int primerNumero, segundoNumero;
+        String respuesta;
 
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese primer número");
+        primerNumero = scanner.nextInt();
+        System.out.println("Ingrese segundo número");
+        segundoNumero = scanner.nextInt();
 
         //Proceso
+        if (primerNumero == segundoNumero) {
+            respuesta = "iguales";
+        } else {
+            respuesta = "diferentes";
+        }
 
         //Salida
-
+        System.out.println("Los números ingresados son " + respuesta);
 
 
     }
