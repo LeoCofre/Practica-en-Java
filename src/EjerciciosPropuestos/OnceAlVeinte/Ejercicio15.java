a33b22afa3fd710b2e8efa5b1a9b3673fde2a2d0
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-29 23:24:47 -0300

    Ejercicio propuesto numero quince.

--- src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio15.java
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio15.java
@@ -1,5 +1,7 @@
 package EjerciciosPropuestos.OnceAlVeinte;
 
+import java.util.Scanner;
+
 public class Ejercicio15 {
 
     public static void main(String[] args) {
@@ -7,8 +9,22 @@ public class Ejercicio15 {
        /*Crear un programa que al ingresar tres números enteros devuelva los números ordenados en
          forma ascendente y en forma descendente.*/
 
-        //Variables
-        int primerNumero, segundoNumero, tercerNumero;
+
+    }
+
+    //Variables
+    int primerNumero, segundoNumero, tercerNumero;
+    Scanner entrada = new Scanner(System.in);
+
+    public void ingresarnumeros() {
+        System.out.println("Por favor ingrese 3 numeros");
+        System.out.print("Ingrese numero 1: ");
+        primerNumero = entrada.nextInt();
+        System.out.print("Ingrese numero 2: ");
+        segundoNumero = entrada.nextInt();
+        System.out.print("Ingrese numero 3: ");
+        tercerNumero = entrada.nextInt();
+
 
     }
 }
