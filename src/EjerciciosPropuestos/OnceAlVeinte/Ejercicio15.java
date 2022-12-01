6e09a0aeb019dbd2d511137c1734d7356b6d8414
Leonardo Cofré <leocofre1981@gmail.com>
2022-11-30 22:35:35 -0300

    Ejercicio propuesto numero quince.

--- src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio15.java
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio15.java
@@ -6,11 +6,59 @@ class Ejercicio15 {
 
     public static void main(String[] args) {
 
+        /*Crear un programa que al ingresar tres números enteros devuelva los números ordenados en
+        forma ascendente y en forma descendente.*/
 
-        Ejercicio15 fc = new Ejercicio15();
-        fc.ingresarnumeros();
-        fc.ordenar();
+        //Variables
+        int primerNumero, segundoNumero, tercerNumero;
+        String ordenAscendente, ordenDesendente;
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor ingrese 3 numeros");
+        System.out.print("Ingrese numero 1: ");
+        primerNumero = scanner.nextInt();
+        System.out.print("Ingrese numero 2: ");
+        segundoNumero = scanner.nextInt();
+        System.out.print("Ingrese numero 3: ");
+        tercerNumero = scanner.nextInt();
+
+        //Proceso
+        if (primerNumero > segundoNumero) {
+            if (primerNumero > tercerNumero) {
+                if (segundoNumero > tercerNumero) {
+                    ordenAscendente = ("Orden Ascendente: " + tercerNumero + " " + segundoNumero + " " + primerNumero);
+                    ordenDesendente = ("Orden descendente: " + primerNumero + " " + segundoNumero + " " + tercerNumero);
+                } else {
+                    ordenAscendente = ("Orden Ascendente: " + primerNumero + " " + tercerNumero + " " + segundoNumero);
+                   ordenDesendente = ("Orden descendente: " + segundoNumero + " " + tercerNumero + " " + primerNumero);
+                }
+            } else {
+                ordenAscendente = ("Orden Ascendente: " + segundoNumero + " " + primerNumero + " " + tercerNumero);
+                System.out.println("Orden descendente: " + tercerNumero + " " + primerNumero + " " + segundoNumero);
+            }
+        } else {
+            if (segundoNumero > tercerNumero) {
+                if (primerNumero > tercerNumero) {
+                    ordenAscendente = ("Orden Ascendente: " + tercerNumero + " " + primerNumero + " " + segundoNumero);
+                    ordenDesendente = ("Orden descendente: " + segundoNumero + " " + primerNumero + " " + tercerNumero);
+                } else {
+                    ordenAscendente = ("Orden Ascendente: " + primerNumero + " " + tercerNumero + " " + segundoNumero);
+                    ordenDesendente = ("Orden descendente: " + segundoNumero + " " + tercerNumero + " " + primerNumero);
+                }
+            } else {
+                ordenAscendente = ("Orden Ascendente: " + primerNumero + " " + segundoNumero + " " + tercerNumero);
+                ordenDesendente = ("Orden descendente: " + tercerNumero + " " + segundoNumero + " " + primerNumero);
+            }
+        }
     }
+
+
+
+        /*Ejercicio15 fc = new Ejercicio15();
+        fc.ingresarnumeros();
+        fc.ordenar();*/
+    /*}
 
     int a, b, c;
 
@@ -54,7 +102,5 @@ class Ejercicio15 {
                 System.out.println("Orden descendente: " + c + " " + b + " " + a);
             }
         }
-    }
-
-
+    }*/
 }
