d2e2708044f02d8287c0f64b80e2553917c1e0cf
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-01 23:18:18 -0300

    Ejercicio propuesto numero diesiseis.

--- src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio16.java
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio16.java
@@ -4,6 +4,10 @@ public class Ejercicio16 {
 
     public static void main(String[] args) {
 
-        //
+        /*Después de ingresar 4 notas, obtener el promedio de la tres mejores. Mostrar el mensaje
+        «Aprobado>> si el promedio es mayor o igual a 11; caso contrario, mostrar «Desaprobado».*/
+
+        //Variables
+
     }
 }
