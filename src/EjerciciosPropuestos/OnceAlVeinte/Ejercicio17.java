d16d0ee13a1da9db127840d17df481150947cc07
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-02 23:58:03 -0300

    Ejercicio propuesto numero diecisiete.

---
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio17.java
@@ -0,0 +1,9 @@
+package EjerciciosPropuestos.OnceAlVeinte;
+
+public class Ejercicio17 {
+
+    public static void main(String[] args) {
+        /*Dados los siguientes datos de entrada: saldo anterior, tipo de movimiento «R» (retiro) o «D»
+        (deposito) y monto de la transacción, obtener como dato de salida el saldo actual.*/
+    }
+}
