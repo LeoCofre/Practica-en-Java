04974a78eba322b0e9d20877860885ba95fb9246
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-04 23:04:57 -0300

    Ejercicio propuesto numero diecisiete.

--- src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio17.java
+++ src/EjerciciosPropuestos/OnceAlVeinte/Ejercicio17.java
@@ -5,24 +5,36 @@ import java.util.Scanner;
 public class Ejercicio17 {
 
     public static void main(String[] args) {
+
         /*Dados los siguientes datos de entrada: saldo anterior, tipo de movimiento «R» (retiro) o «D»
         (deposito) y monto de la transacción, obtener como dato de salida el saldo actual.*/
 
         //Variables
-        //int saldoAnterior, tipoMovimiento, retiro, movimiento, monto, saldoActual;
-
-        int sa,mt,sactual=0;
-        char tm;
+        int montoAnterior, montoTransaccion, saldoActual = 0;
+        char tipoMovimiento;
 
         //Entrada
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Bienvenido ");
-        System.out.println("Elija una opcíon: ");
-        System.out.println("R : Retirar" );
-        System.out.println("D : Depositar");
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Ingrese Saldo");
+        montoAnterior = teclado.nextInt();
+        System.out.print("Ingrese el Tipo de Movimiento");
+        tipoMovimiento = teclado.next().charAt(0);
+        System.out.print("IngreseMonto");
+        tipoMovimiento = teclado.next().charAt(0);
 
         //Proceso
-
+        if (tipoMovimiento == 'R') {
+            saldoActual = montoAnterior - tipoMovimiento;
+        } else {
+            if (tipoMovimiento == 'D') {
+                saldoActual = montoAnterior + tipoMovimiento;
+            }
+        }
+        //Salida
+        System.out.println("");
+        System.out.print("Es: " + saldoActual);
     }
 
 }
+
+
