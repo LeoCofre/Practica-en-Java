0935b1c960b2c56b33c2eba6aa37c5915112a0fa
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-05 23:02:15 -0300

    Ejercicio propuesto numero diecinueve.

---
+++ src/EjerciciosPropuestos/OnceAlVeinte/Propuesto19.java
@@ -0,0 +1,15 @@
+package EjerciciosPropuestos.OnceAlVeinte;
+
+public class Propuesto19 {
+
+    public static void main(String[] args) {
+
+         /*Dadas tres longitudes, diga si forman un triángulo.
+         TEOREMA: En todo triángulo cada lado es menor que la suma de los otros dos, pero mayor que su
+         diferencia. */
+
+        //Variables
+        int ladoUno, ladoDos, ladotres;
+
+    }
+}
