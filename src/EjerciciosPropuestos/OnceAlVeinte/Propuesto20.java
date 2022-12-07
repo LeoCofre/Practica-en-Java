6205161067292e938a0bea34dc9ed42b9224ecaf
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-06 23:59:32 -0300

    Ejercicio propuesto numero veinte.

--- src/EjerciciosPropuestos/OnceAlVeinte/Propuesto20.java
+++ src/EjerciciosPropuestos/OnceAlVeinte/Propuesto20.java
@@ -3,5 +3,11 @@ package EjerciciosPropuestos.OnceAlVeinte;
 public class Propuesto20 {
 
     public static void main(String[] args) {
+
+        /*Dadas tres longitudes, si forman un triángulo, devolver el tipo de triángulo según sus lados.
+                T. Equilátero: Sus tres lados son iguales
+        T. Isósceles: Tiene dos lados iguales.
+        T. Escaleno: Sus tres lados son diferentes.*/
+
     }
 }
