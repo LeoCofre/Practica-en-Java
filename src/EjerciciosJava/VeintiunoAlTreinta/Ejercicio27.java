4e8733e469b5a6fc266e7348fca1be1ab76973a6
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-08 13:40:26 -0300

    Ejercicio propuesto numero veintisiete.

--- src/EjerciciosJava/VeintiunoAlTreinta/Ejercicio27.java
+++ src/EjerciciosJava/VeintiunoAlTreinta/Ejercicio27.java
@@ -8,12 +8,49 @@ public class Ejercicio27 {
 
         //Dado un número entero de un dígito (O al 9), devolver el número en letras.
 
-         //Variables
+        //Variables
         int numero;
         String numeroEnLetras = "";
 
         //Entrada
         Scanner teclado = new Scanner(System.in);
         System.out.println("Ingrese un número del 0 al 9");
+        numero = teclado.nextInt();
+
+        //Proceso
+        switch (numero) {
+            case 0:
+
+                numeroEnLetras = "CERO";
+                break;
+            case 1:
+                numeroEnLetras = "UNO";
+                break;
+            case 2:
+                numeroEnLetras = "DOS";
+                break;
+            case 3:
+                numeroEnLetras = " TRES";
+                break;
+            case 4:
+                numeroEnLetras = "CUATRO";
+                break;
+            case 5:
+                numeroEnLetras = "CINCO";
+                break;
+            case 6:
+                numeroEnLetras = " SEIS";
+                break;
+            case 7:
+                numeroEnLetras = " SIETE";
+                break;
+            case 8:
+                numeroEnLetras = "OCHO";
+                break;
+            case 9:
+                numeroEnLetras = " NUEVE";
+                break;
+
+        }
     }
 }
