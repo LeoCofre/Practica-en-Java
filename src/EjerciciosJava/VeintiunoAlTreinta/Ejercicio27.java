f392e8e868af795049c88672a9ed511cf73120fd
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-08 00:42:53 -0300

    Ejercicio propuesto numero veintisiete.

---
+++ src/EjerciciosJava/VeintiunoAlTreinta/Ejercicio27.java
@@ -0,0 +1,19 @@
+package EjerciciosJava.VeintiunoAlTreinta;
+
+import java.util.Scanner;
+
+public class Ejercicio27 {
+
+    public static void main(String[] args) {
+
+        //Dado un número entero de un dígito (O al 9), devolver el número en letras.
+
+         //Variables
+        int numero;
+        String numeroEnLetras = "";
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese un número del 0 al 9");
+    }
+}
