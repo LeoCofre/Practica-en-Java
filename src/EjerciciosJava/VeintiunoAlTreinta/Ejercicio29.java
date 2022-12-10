90d3bc1d6d9000733bd78160b19669fc65e72f12
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-09 23:33:28 -0300

    Ejercicio numero veintiocho.

---
+++ src/EjerciciosJava/VeintiunoAlTreinta/Ejercicio29.java
@@ -0,0 +1,31 @@
+package EjerciciosJava.VeintiunoAlTreinta;
+
+import java.util.Scanner;
+
+public class Ejercicio29 {
+
+    public static void main(String[] args) {
+
+        //Dada una letra, determinar si es una vocal.
+
+        //Variables
+        char letra;
+        String respuesta = "";
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese una letra: ");
+        letra = teclado.next().charAt(0);
+
+        //Proceso
+        if (letra == 'a' || letra == 'A')
+            respuesta = "La letra ingresada es una vocal";
+        else if (letra == 'e' || letra == 'E') {
+            respuesta = "La letra ingresada es una vocal";
+            else if (letra == 'i' || letra == 'I') {
+                respuesta = "La letra ingresada es una vocal";
+            }
+
+        }
+    }
+}
