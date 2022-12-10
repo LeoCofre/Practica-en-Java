be303efc2d2221e4d9261baabbdb2f64865e8daf
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-10 20:08:53 -0300

    Ejercicio numero treinta.

--- src/EjerciciosJava/VeintiunoAlTreinta/Ejercicio30.java
+++ src/EjerciciosJava/VeintiunoAlTreinta/Ejercicio30.java
@@ -4,5 +4,14 @@ public class Ejercicio30 {
 
     public static void main(String[] args) {
 
+        /*Al ingresar el número de un mes, devolver la estación del año de acuerdo a la siguiente tabla.
+        Mes Estación
+        1, 2, 3 Verano
+        4, 5, 6 Otoño
+        7, 8, 9 Invierno
+        10, 11, 12 Primavera*/
+
+        //Variables
+
     }
 }
