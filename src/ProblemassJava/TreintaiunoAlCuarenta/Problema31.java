f4a670741d40946a0f8f3dbf913935e3f2acd59d
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-13 00:20:06 -0300

    Ejercicio numero treinta.

---
+++ src/ProblemassJava/TreintaiunoAlCuarenta/Problema31.java
@@ -0,0 +1,39 @@
+package ProblemassJava.TreintaiunoAlCuarenta;
+
+import java.util.Scanner;
+
+public class Problema31 {
+
+    public static void main(String[] args) {
+
+        /*Dado la nota promedio de un alumno, obtener la categoría, según la siguiente tabla.
+
+            Promedio Categoría
+            Entre O Y 5 Pésimo
+            Entre 6 Y 10 Malo
+            Entre 11 Y 14 Regular
+            Entre 15 Y 17 Bueno
+            Entre 18 y 20 Excelente*/
+
+        //Variables
+        int promedioNotas;
+        String categoria;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese promedio de notas obtenido");
+        promedioNotas = teclado.nextInt();
+
+        //Proceso
+        switch (promedioNotas) {
+            case 1:
+            case 2:
+            case 3:
+
+
+        }
+
+
+    }
+}
+
