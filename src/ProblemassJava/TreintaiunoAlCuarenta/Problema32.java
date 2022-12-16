acafc568ac9fe373a4c6c519cafdb739072d8692
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-15 23:57:08 -0300

    Ejercicio numero treintaidos.

--- src/ProblemassJava/TreintaiunoAlCuarenta/Problema32.java
+++ src/ProblemassJava/TreintaiunoAlCuarenta/Problema32.java
@@ -15,47 +15,51 @@ public class Problema32 {
          Primavera               Del 23 de septiembre al 20 de diciembre*/
 
         //Variables
-        int d, m;
-        String e = "";
+        int dia, mes;
+        String estacion = "";
 
         //Entrada
         Scanner teclado = new Scanner(System.in);
         System.out.println("Ingrese día: ");
-        d = teclado.nextInt();
+        dia = teclado.nextInt();
         System.out.println("Ingrese mes: ");
-        m = teclado.nextInt();
+        mes = teclado.nextInt();
 
         //Proceso
-        switch (m) {
+        switch (mes) {
             case 1:
             case 2:
             case 3:
-                e = " VERANO";
-                if (m == 3 && d > 20)
-                    e = "OTOÑO";
+                estacion = " VERANO";
+                if (mes == 3 && dia > 20)
+                    estacion = "OTOÑO";
                 break;
             case 4:
             case 5:
             case 6:
-                e = "OTOÑO";
-                if (m == 6 && d > 21)
-                    e = " INVIERNO";
+                estacion = "OTOÑO";
+                if (mes == 6 && dia > 21)
+                    estacion = " INVIERNO";
                 break;
             case 7:
             case 8:
             case 9:
-                e = " INVIERNO";
-                if (m == 9 && d > 22)
-                    e = "PRIMAVERA";
+                estacion = " INVIERNO";
+                if (mes == 9 && dia > 22)
+                    estacion = "PRIMAVERA";
                 break;
             case 10:
             case 11:
             case 12:
-                e = " PRIMAVERA";
-                if (m == 12 && d > 20)
-                    e = " VERANO";
+                estacion = " PRIMAVERA";
+                if (mes == 12 && dia > 20)
+                    estacion = " VERANO";
                 break;
 
         }
+
+        //Salida
+        System.out.println("");
+        System.out.println("La estación es: " + estacion);
     }
 }
