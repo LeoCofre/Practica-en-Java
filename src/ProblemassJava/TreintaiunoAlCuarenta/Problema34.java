a2931c3894d0ec219035807fd7b5000975d9a54b
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-18 18:08:02 -0300

    Ejercicio numero treintaicuatro.

---
+++ src/ProblemassJava/TreintaiunoAlCuarenta/Problema34.java
@@ -0,0 +1,33 @@
+package ProblemassJava.TreintaiunoAlCuarenta;
+
+import java.util.Scanner;
+
+public class Problema34 {
+
+    public static void main(String[] args) {
+
+             /*Determine el importe a pagar para el examen de admisión de una universidad, cuyo valor
+             depende del nivel socioeconómico y el colegio de procedencia. */
+
+        /*                      Nivel Social
+            Colegio              A   B   C
+            Nacional            300 200 100
+            Particular          400 300 200
+         */
+
+        //Variables
+        char c,n;
+        int mp = 0;
+
+        //Entrada
+        Scanner teclado= new Scanner(System. in) ;
+        System.out.print("Colegio: ");
+        c = teclado.next().charAt(0);
+        System.out.print("Nivel: ");
+        n = teclado.next().charAt(0);
+
+        //Proceso
+
+    }
+}
+
