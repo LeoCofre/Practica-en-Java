83afc8f64f0882f168667c8d68af7020d64770b5
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-27 23:59:04 -0300

    Ejercicio numero treintaiocho.

--- src/ProblemasJava/TreintaiunoAlCuarenta/Problema38.java
+++ src/ProblemasJava/TreintaiunoAlCuarenta/Problema38.java
@@ -5,5 +5,9 @@ public class Problema38 {
     public static void main(String[] args) {
 
         //Dado el día, mes y año, determine si es una fecha correcta, considere los años bisiestos.
+
+        //Variables
+
+
     }
 }
