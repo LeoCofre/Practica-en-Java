dc52c13d1b4e6b158ead0150088c6928b8785954
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-27 23:36:36 -0300

    Ejercicio numero treintaisiete.

--- src/ProblemasJava/TreintaiunoAlCuarenta/Problema37.java
+++ src/ProblemasJava/TreintaiunoAlCuarenta/Problema37.java
@@ -1,8 +1,33 @@
 package ProblemasJava.TreintaiunoAlCuarenta;
 
+import java.util.Scanner;
+
 public class Problema37 {
 
     public static void main(String[] args) {
+
+        /*Diseñe un algoritmo que califique el puntaje obtenido en el lanzamiento de tres dados en
+        base a la cantidad de seis obtenidos, de acuerdo a lo siguiente:
+        Tres seis: Oro
+        Dos seis: Plata
+        Un seis: Bronce
+        Ningún seis: Perdió*/
+
+        //Variables
+        int primerDado, segundoDado, tercerDado;
+        String premio = "";
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Primer dado: ");
+        primerDado = teclado.nextInt();
+        System.out.print("Segundo dado: ");
+        segundoDado = teclado.nextInt();
+        System.out.print("Tercer dado: ");
+        tercerDado = teclado.nextInt();
+
+
+
 
 
     }
