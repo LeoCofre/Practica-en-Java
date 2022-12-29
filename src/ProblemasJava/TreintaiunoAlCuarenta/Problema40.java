bbbcd6a4cb57b19adf2c351a8d29244df0809472
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-29 00:47:17 -0300

    Ejercicio numero cuarenta.

---
+++ src/ProblemasJava/TreintaiunoAlCuarenta/Problema40.java
@@ -0,0 +1,12 @@
+package ProblemasJava.TreintaiunoAlCuarenta;
+
+public class Problema40 {
+
+    public static void main(String[] args) {
+
+        //Convierta a números romanos, números menores a 4000.
+
+        //Variables
+
+    }
+}
