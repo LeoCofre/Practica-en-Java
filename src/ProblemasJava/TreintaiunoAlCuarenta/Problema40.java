7d485eab2b8278502547277d6a32683b69fb951d
Leonardo Cofré <leocofre1981@gmail.com>
2022-12-30 23:50:36 -0300

    Ejercicio numero cuarenta.

--- src/ProblemasJava/TreintaiunoAlCuarenta/Problema40.java
+++ src/ProblemasJava/TreintaiunoAlCuarenta/Problema40.java
@@ -38,6 +38,93 @@ public class Problema40 {
                     respuesta = "MMM";
                     break;
             }
+            switch (centena) {
+                case 1:
+                    respuesta = "C";
+                    break;
+                case 2:
+                    respuesta = "CC";
+                    break;
+                case 3:
+                    respuesta = "CCC";
+                    break;
+                case 4:
+                    respuesta = "CD";
+                    break;
+                case 5:
+                    respuesta = "D";
+                    break;
+                case 6:
+                    respuesta = "DC";
+                    break;
+                case 7:
+                    respuesta = "DCC";
+                    break;
+                case 8:
+                    respuesta = "DCCC";
+                    break;
+                case 9:
+                    respuesta = "CM";
+                    break;
+            }
+            switch (decena) {
+                case 1:
+                    respuesta = "X";
+                    break;
+                case 2:
+                    respuesta = "XX";
+                    break;
+                case 3:
+                    respuesta = "XXX";
+                    break;
+                case 4:
+                    respuesta = "XL";
+                    break;
+                case 5:
+                    respuesta = "L";
+                    break;
+                case 6:
+                    respuesta = "LX";
+                    break;
+                case 7:
+                    respuesta = "LXX";
+                    break;
+                case 8:
+                    respuesta = "LXXX";
+                    break;
+                case 9:
+                    respuesta = "XC";
+                    break;
+            }
+            switch (unidad){
+                case 1:
+                    respuesta = "I";
+                    break;
+                case 2:
+                    respuesta = "II";
+                    break;
+                case 3:
+                    respuesta = "III";
+                    break;
+                case 4:
+                    respuesta = "IV";
+                    break;
+                case 5:
+                    respuesta = "V";
+                    break;
+                case 6:
+                    respuesta = "VI";
+                    break;
+                case 7:
+                    respuesta = "VII";
+                    break;
+                case 8:
+                    respuesta = "VIII";
+                    break;
+                case 9:
+                    respuesta = "IX";
+                    break;
+            }
         }
 
 
