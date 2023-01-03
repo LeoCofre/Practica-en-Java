f70bb819880941807cb0cacb56044d482b396602
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-02 23:32:37 -0300

    Ejercicio propuesto numero 23.

---
+++ src/EjerciciosPropuestos/VentiunoAlTreinta/Propuesto23.java
@@ -0,0 +1,23 @@
+package EjerciciosPropuestos.VentiunoAlTreinta;
+
+import java.util.Scanner;
+
+public class Propuesto23 {
+
+    public static void main(String[] args) {
+
+        //Dados los siguientes operadores aritméticos(+,-,* y/), devuelva el nombre del operador.
+
+        //Variables
+        char operador;
+        String nombreOperador = "";
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese uno de los siguientes operadores: +,-,* ,/");
+        operador = scanner.next().charAt(0);
+
+        //Proceso
+
+    }
+}
