9273307199cca834c08e32eb905f80b1000496fa
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-02 23:03:33 -0300

    Ejercicio propuesto numero 21.

--- src/EjerciciosPropuestos/VentiunoAlTreinta/Propuesto21.java
+++ src/EjerciciosPropuestos/VentiunoAlTreinta/Propuesto21.java
@@ -18,7 +18,47 @@ public class Propuesto21 {
         numeroMes = scanner.nextInt();
 
         //Proceso
-
+        switch (numeroMes) {
+            case 1:
+                mesEnLetras = "Enero";
+                break;
+            case 2:
+                mesEnLetras = "Febrero";
+                break;
+            case 3:
+                mesEnLetras = "Marzo";
+                break;
+            case 4:
+                mesEnLetras = "Abril";
+                break;
+            case 5:
+                mesEnLetras = "Mayo";
+                break;
+            case 6:
+                mesEnLetras = "Junio";
+                break;
+            case 7:
+                mesEnLetras = "Julio";
+                break;
+            case 8:
+                mesEnLetras = "Agosto";
+                break;
+            case 9:
+                mesEnLetras = "Septiembre";
+                break;
+            case 10:
+                mesEnLetras = "Octubre";
+                break;
+            case 11:
+                mesEnLetras = "Noviembre";
+                break;
+            case 12:
+                mesEnLetras = "Diciembre";
+                break;
+        }
+        //Salida
+        System.out.println();
+        System.out.println("El mes ingresado es: " + mesEnLetras);
 
     }
 }
