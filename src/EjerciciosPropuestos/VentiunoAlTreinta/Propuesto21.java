906b277e2a30ef7762f45d2da0a4fcf4c8e1a656
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-02 22:56:18 -0300

    Ejercicio propuesto numero 21.

--- src/EjerciciosPropuestos/VentiunoAlTreinta/Propuesto21.java
+++ src/EjerciciosPropuestos/VentiunoAlTreinta/Propuesto21.java
@@ -1,8 +1,24 @@
 package EjerciciosPropuestos.VentiunoAlTreinta;
 
+import java.util.Scanner;
+
 public class Propuesto21 {
 
     public static void main(String[] args) {
+
+        //Dado el número de un mes, devolver el mes en letras.
+
+        //Variables
+        int numeroMes;
+        String mesEnLetras = "";
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese número de mes: ");
+        numeroMes = scanner.nextInt();
+
+        //Proceso
+
 
     }
 }
