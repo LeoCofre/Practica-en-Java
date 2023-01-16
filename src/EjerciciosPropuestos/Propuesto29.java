7e541ed0024bc832562b70761a18dd6e403e01a8
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-15 23:59:37 -0300

    Ejercicio propuesto numero veitiocho.

---
+++ src/EjerciciosPropuestos/Propuesto29.java
@@ -0,0 +1,23 @@
+package EjerciciosPropuestos;
+
+public class Propuesto29 {
+
+    public static void main(String[] args) {
+
+        /*Según la siguiente tabla, obtener la ciudad que visitará, después de ingresar su sexo y el
+        puntaje obtenido en un examen.
+
+                                    _________Sexo___________
+            Puntaje                 Masculino       Femenino
+       Entre 18 y 35                Arequipa        Cuzco
+       Entre 36 y 75                Cuzco           Iquitos
+       Mayor a 75                   Iquitos         Arequipa
+
+        */
+
+        //Variables
+        int puntaje, sexo;
+        String cuidad = "";
+    }
+
+}
