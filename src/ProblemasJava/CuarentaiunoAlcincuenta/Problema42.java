31476364635b5df8399a9262e892f0e6bd0e4368
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-20 00:17:33 -0300

    Ejercicio 42.

--- src/ProblemasJava/CuarentaiunoAlcincuenta/Problema42.java
+++ src/ProblemasJava/CuarentaiunoAlcincuenta/Problema42.java
@@ -1,4 +1,32 @@
 package ProblemasJava.CuarentaiunoAlcincuenta;
 
+import java.util.Scanner;
+
 public class Problema42 {
+
+    public static void main(String[] args) {
+
+        //Dado un rango de números enteros, obtener la cantidad de números enteros que contiene.
+
+        //Variables
+        int iterador, numeroInicial, numeroFinal, cantidad = 0;
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese número inicial");
+        numeroInicial = scanner.nextInt();
+        System.out.println("Ingrese número final");
+        numeroFinal = scanner.nextInt();
+
+        //Proceso
+        iterador = numeroInicial + 1;
+        while (iterador < numeroFinal){
+            cantidad += 1;
+            iterador ++;
+        }
+
+        //Salida
+        System.out.println();
+        System.out.println("Catidad de números: " + cantidad);
+    }
 }
