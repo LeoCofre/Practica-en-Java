f3827116d7f3d42705c9f06f8978cb1b58cf6ffd
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-22 01:04:28 -0300

    Ejercicio 43.

--- src/ProblemasJava/CuarentaiunoAlcincuenta/Problema43.java
+++ src/ProblemasJava/CuarentaiunoAlcincuenta/Problema43.java
@@ -1,11 +1,33 @@
 package ProblemasJava.CuarentaiunoAlcincuenta;
 
+import java.util.Scanner;
+
 public class Problema43 {
 
     public static void main(String[] args) {
 
         // Dado un rango de números enteros, obtener la cantidad de números pares que contiene.
 
+        //Variables
+        int iterador, numeroInicial, numeroFinal, cantidadPares = 0;
 
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Numero inicial: ");
+        numeroInicial = teclado.nextInt();
+        System.out.print("Numero final: ");
+        numeroFinal = teclado.nextInt();
+
+        //Proceso
+        iterador = numeroInicial + 1;
+        while (iterador < numeroFinal) {
+            if (iterador % 2 == 0) {
+                cantidadPares += 1;
+            }
+            iterador++;
+        }
+        //Salida
+        System.out.println("");
+        System.out.println("Cantidad de pares: " + cantidadPares);
     }
 }
