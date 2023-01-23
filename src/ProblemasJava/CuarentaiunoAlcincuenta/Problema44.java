476413c1fbf7563783f57ca2dcd768ca773ca9c1
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-22 21:58:09 -0300

    Ejercicio 44.

--- src/ProblemasJava/CuarentaiunoAlcincuenta/Problema44.java
+++ src/ProblemasJava/CuarentaiunoAlcincuenta/Problema44.java
@@ -4,9 +4,12 @@ public class Problema44 {
 
     public static void main(String[] args) {
 
-       // Obtener la cantidad de los primeros N números múltiplos de 5.
+        // Obtener la cantidad de los primeros N números múltiplos de 5.
+
+        //Variabl es
 
 
+        int i, ni, nf, cp = 0;
 
 
     }
