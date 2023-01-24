6edc5cca807ff91bae3d339cb3f39b05ec57500c
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-23 23:22:36 -0300

    Ejercicio 44.

--- src/ProblemasJava/CuarentaiunoAlcincuenta/Problema44.java
+++ src/ProblemasJava/CuarentaiunoAlcincuenta/Problema44.java
@@ -1,15 +1,32 @@
 package ProblemasJava.CuarentaiunoAlcincuenta;
 
+import java.util.Scanner;
+
 public class Problema44 {
 
     public static void main(String[] args) {
 
         // Obtener la cantidad de los primeros N números múltiplos de 5.
 
-        //Variabl es
+        //Variables
+        int iterador, numero, cantidad = 0;
 
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print(" Numero : ");
+        numero = teclado.nextInt();
 
-        int i, ni, nf, cp = 0;
+        //Proceso
+        iterador = 1;
+        while (iterador <= numero) {
+            if (iterador % 5 == 0) {
+                cantidad += 1;
+            }
+            iterador++;
+        }
+        // Salida
+        System.out.println("");
+        System.out.println("Cantidad: " + cantidad);
 
 
     }
