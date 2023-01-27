6ecf21f5806a015f3368f3ac5da4e19a65b042fe
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-26 23:50:21 -0300

    Ejercicio 48.

--- src/ProblemasJava/CuarentaiunoAlCincuenta/Problema47.java
+++ src/ProblemasJava/CuarentaiunoAlCincuenta/Problema47.java
@@ -1,4 +1,4 @@
-package ProblemasJava.CuarentaiunoAlcincuenta;
+package ProblemasJava.CuarentaiunoAlCincuenta;
 
 import java.util.Scanner;
 
---
+++ src/ProblemasJava/CuarentaiunoAlCincuenta/Problema48.java
@@ -0,0 +1,15 @@
+package ProblemasJava.CuarentaiunoAlCincuenta;
+
+public class Problema48 {
+
+    public static void main(String[] args) {
+
+        /*Dados dos números, diga si son amigos. Recuerde que dos números son amigos si la suma de
+        los divisores de uno de ellos es igual al otro y viceversa; por ejemplo, 220 y 284 son amigos:
+        Divisores de 220 son:
+        1 + 2 + 4 + 5 + 10 + 11 + 20 + 22 + 44 + 55 + 110 = 284
+        Divisores de 284 son:
+        1 + 2 + 4 + 71 + 142 = 220*/
+    }
+
+}
