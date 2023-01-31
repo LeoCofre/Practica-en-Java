2990e4647ae45cdb999e1fd3d684a10ef81537b5
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-31 19:13:11 -0300

    Ejercicio 49.

---
+++ src/ProblemasJava/CuarentaiunoAlCincuenta/Problema49.java
@@ -0,0 +1,9 @@
+package ProblemasJava.CuarentaiunoAlCincuenta;
+
+public class Problema49 {
+
+    public static void main(String[] args) {
+
+        //Dado un número, devuelva el inverso del número.
+    }
+}
