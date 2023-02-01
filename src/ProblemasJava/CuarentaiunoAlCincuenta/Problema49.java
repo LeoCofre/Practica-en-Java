fb7f9f62c027ba6e48ac1b80e272815079575384
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-31 22:27:16 -0300

    Ejercicio 49.

--- src/ProblemasJava/CuarentaiunoAlCincuenta/Problema49.java
+++ src/ProblemasJava/CuarentaiunoAlCincuenta/Problema49.java
@@ -1,9 +1,30 @@
 package ProblemasJava.CuarentaiunoAlCincuenta;
 
+import java.util.Scanner;
+
 public class Problema49 {
 
     public static void main(String[] args) {
 
         //Dado un número, devuelva el inverso del número.
+
+        //Variables
+        int devolver, numero, inverso = 0;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Ingrese numero: ");
+        numero = teclado.nextInt();
+
+        //Proceso
+        while (numero > 0) {
+            devolver = numero % 10;
+            numero = numero / 10;
+            inverso = inverso * 10 + devolver;
+        }
+
+        //Salida
+        System.out.println("");
+        System.out.println("Numero Inverso : " + inverso);
     }
 }
