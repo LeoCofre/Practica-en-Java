0af0990d5dc841e654a8d97705ec4b16cee9f871
Leonardo Cofré <leocofre1981@gmail.com>
2023-01-31 23:20:51 -0300

    Ejercicio 50.

--- src/ProblemasJava/CuarentaiunoAlCincuenta/Problema50.java
+++ src/ProblemasJava/CuarentaiunoAlCincuenta/Problema50.java
@@ -7,32 +7,32 @@ public class Problema50 {
     public static void main(String[] args) {
 
         /*Crear un algoritmo que indique si un número es cubo perfecto (anstrong) o no, se dice que
-        un número es cubo perfecto si al sumar los cubos de sus dígitos dan el mismo número, por ejemplo 153,
-                cubos de sus dígitos 13 + 53 + 33 = 153 el número 153 es cubo perfecto.*/
+        un número es cubo perfecto si al sumar los cubos de sus dígitos dan el mismo número, por ejemplo
+        153, cubos de sus dígitos 13 + 53 + 33 = 153 el número 153 es cubo perfecto.*/
 
         //Variables
-        int t, d, s = 0, n;
-        String r;
+        int t, digitos, suma = 0, numero;
+        String resultado;
 
         //Entrada
         Scanner teclado = new Scanner(System.in);
         System.out.print("Numero: ");
-        n = teclado.nextInt();
+        numero = teclado.nextInt();
 
         //Proceso
-        t = n;
+        t = numero;
         while (t > 0) {
-            d = t % 10;
+            digitos = t % 10;
             t /= 10;
-            s = (int) (s + Math.pow(d, 3.0));
+            suma = (int) (suma + Math.pow(digitos, 3.0));
         }
-        if (n == s)
-            r = " CUBO PERFECTO";
+        if (numero == suma)
+            resultado = "CUBO PERFECTO";
         else
-            r = " NO ES CUBO PERFECTO";
+            resultado = "NO ES CUBO PERFECTO";
 
         // Salida
         System.out.println();
-        System.out.println("Resultado: " + r);
+        System.out.println("Resultado: " + resultado);
     }
 }
