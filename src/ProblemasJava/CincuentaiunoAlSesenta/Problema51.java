ecba276820fee0623f7141c5ac47ef747c2a10c7
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-01 23:52:21 -0300

    Ejercicio 50.

---
+++ src/ProblemasJava/CincuentaiunoAlSesenta/Problema51.java
@@ -0,0 +1,19 @@
+package ProblemasJava.CincuentaiunoAlSesenta;
+
+public class Problema51 {
+
+    public static void main(String[] args) {
+
+       /* Obtenga el cociente y el residuo de una división mediante restas sucesivas; por ejemplo, si el
+        dividendo es 3989 y el divisor es 1247, entonces:
+
+        3989 - 1247 = 2742 R(l )
+        2742 - 1247 = 1495 R(2)
+        1495 - 1247 = 248 R(3)
+
+        Ya no se puede seguir restando, pues 248 es menor a 1247, entonces el cociente es el número de veces
+        restado (3) y el residuo es el último número obtenido (248).*/
+
+
+    }
+}
