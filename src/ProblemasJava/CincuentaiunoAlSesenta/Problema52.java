229ee0d2b430e2dab80827d1665e208c4a200bd6
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-07 23:03:00 -0300

    Ejercicio 52.

--- src/ProblemasJava/CincuentaiunoAlSesenta/Problema52.java
+++ src/ProblemasJava/CincuentaiunoAlSesenta/Problema52.java
@@ -1,5 +1,7 @@
 package ProblemasJava.CincuentaiunoAlSesenta;
 
+import java.util.Scanner;
+
 public class Problema52 {
 
     public static void main(String[] args) {
@@ -7,5 +9,31 @@ public class Problema52 {
         /*Determine si un número es capicúa o no. Se dice que un número capicúa es aquel que al
         invertir sus cifras da el mismo número; por ejemplo, 12321 invertido es 12321. Entonces es un número
         capicúa . */
+
+        //Variables
+        int n, i = 0, d, t;
+        String r;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Numero:");
+        n = teclado.nextInt();
+
+        //Proceso
+        t = n;
+        while (t > 0) {
+            d = t % 10;
+            t = t / 10;
+            i = i * 10 + d;
+        }
+
+        if (n == i)
+            r = "ES CAPICUA";
+        else
+            r = "NO ES CAPICUA";
+
+        // Salida
+        System.out.println();
+        System.out.println("Resultado: " + r);
     }
 }
