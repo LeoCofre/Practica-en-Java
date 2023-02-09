ceae0807ea3a40566ef2fa13426d99f25c002c49
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-09 17:51:52 -0300

    Ejercicio 56.

---
+++ src/ProblemasJava/CincuentaiunoAlSesenta/Problema56.java
@@ -0,0 +1,10 @@
+package ProblemasJava.CincuentaiunoAlSesenta;
+
+public class Problema56 {
+
+    public static void main(String[] args) {
+
+        //Obtener la suma de los primeros N números naturales positivos.
+
+    }
+}
