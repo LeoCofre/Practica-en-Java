5f64f2ce4d8cef0216d0e694098f2b809f258bf7
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-09 17:57:54 -0300

    Ejercicio 56.

--- src/ProblemasJava/CincuentaiunoAlSesenta/Problema56.java
+++ src/ProblemasJava/CincuentaiunoAlSesenta/Problema56.java
@@ -1,10 +1,28 @@
 package ProblemasJava.CincuentaiunoAlSesenta;
 
+import java.util.Scanner;
+
 public class Problema56 {
 
     public static void main(String[] args) {
 
         //Obtener la suma de los primeros N números naturales positivos.
+
+        //Variables
+        int i, n, s = 0;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Numero: ");
+        n = teclado.nextInt();
+
+        //Proceso
+        for (i = 1; i <= n; i++)
+            s = s + i;
+
+        //Salida
+        System.out.println();
+        System.out.println("Suma: " + s);
 
     }
 }
