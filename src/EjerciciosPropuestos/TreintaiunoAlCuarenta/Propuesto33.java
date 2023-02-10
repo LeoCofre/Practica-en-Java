b157b27e30ac5d0864a8582f14f5c11dd7e61455
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-10 17:25:56 -0300

    Propuesto 33.

---
+++ src/EjerciciosPropuestos/TreintaiunoAlCuarenta/Propuesto33.java
@@ -0,0 +1,33 @@
+package EjerciciosPropuestos.TreintaiunoAlCuarenta;
+
+import java.util.Scanner;
+
+public class Propuesto33 {
+
+    public static void main(String[] args) {
+
+        //Calcular la suma y el producto de los N primeros números naturales múltiplos de 3.
+
+        //Variables
+        int numero, suma = 0, producto = 1, contador = 0, iterador = 3;
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese número:");
+        numero = scanner.nextInt();
+
+        //Proceso
+        while (contador < numero) {
+            if (iterador % 3 == 0) {
+                suma += iterador;
+                producto *= iterador;
+                contador++;
+            }
+            iterador++;
+        }
+
+        System.out.println("Suma: " + suma);
+        System.out.println("Producto: " + producto);
+    }
+}
+
