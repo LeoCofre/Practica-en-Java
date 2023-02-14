53b906d632a4143dc065150bfe4b6fd3f607c2b4
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-13 22:34:52 -0300

    Problema58.

--- src/ProblemasJava/CincuentaiunoAlSesenta/Problema58.java
+++ src/ProblemasJava/CincuentaiunoAlSesenta/Problema58.java
@@ -1,10 +1,33 @@
 package ProblemasJava.CincuentaiunoAlSesenta;
 
+import java.util.Scanner;
+
 public class Problema58 {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
         //Dado un rango de números enteros, obtener la cantidad de números pares que contiene.
 
+        //Variables
+        int i, ni, nf, cp = 0;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Número Inicial: ");
+        ni = teclado.nextInt();
+        System.out.print("Número Final: ");
+        nf = teclado.nextInt();
+
+        //Proceso
+        ni = ni + 1;
+        nf = nf - 1;
+        for (i = ni; i <= nf; i++) {
+            if (i % 2 == 0)
+                cp += 1;
+        }
+
+        //Salida
+        System.out.println();
+        System.out.println("Cantidad de números pares : " + cp);
     }
 }
