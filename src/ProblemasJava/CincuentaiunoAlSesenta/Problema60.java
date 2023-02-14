f163b03026c2309a1799c74cc0a9348af4126b05
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-13 23:06:35 -0300

    Problema60.

--- src/ProblemasJava/CincuentaiunoAlSesenta/Problema60.java
+++ src/ProblemasJava/CincuentaiunoAlSesenta/Problema60.java
@@ -9,12 +9,25 @@ public class Problema60 {
         //Obtener la suma de pares e impares de los primeros N números enteros positivos.
 
         //Variables
-        int iterador, numero, sumaPares, sumaImpares;
+        int iterador, numero, sumaPares = 0, sumaImpares = 0;
 
         //Entrada
         Scanner teclado = new Scanner(System.in);
         System.out.println("Ingrese Número:");
         numero = teclado.nextInt();
+
+        //Proceso
+        for (iterador = 1; iterador >= numero; numero += 2) {
+            sumaImpares += iterador;
+        }
+        for (iterador = 2; iterador >= numero; numero += 2) {
+            sumaPares += iterador;
+        }
+
+        //Salida
+        System.out.println();
+        System.out.println("La cantidad de números impares es: " + sumaImpares);
+        System.out.println("La cantidad de números pares es: " + sumaPares);
 
     }
 }
