d25b1697032c2b0f584d18b51a2b38da8e942900
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-13 22:59:30 -0300

    Problema60.

---
+++ src/ProblemasJava/CincuentaiunoAlSesenta/Problema60.java
@@ -0,0 +1,21 @@
+package ProblemasJava.CincuentaiunoAlSesenta;
+
+import java.util.Scanner;
+
+public class Problema60 {
+
+    public static void main(String[] args) {
+
+        //Obtener la suma de pares e impares de los primeros N números enteros positivos.
+
+        //Variables
+        int iterador, numero, sumaPares, sumaImpares;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese Número:");
+        numero = teclado.nextInt();
+
+
+    }
+}
