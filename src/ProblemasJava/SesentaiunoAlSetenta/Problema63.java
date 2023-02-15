c2be4de867da9a0a0ed145676657b51fbb6cd98d
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-15 01:42:58 -0300

    Problema 62.

---
+++ src/ProblemasJava/SesentaiunoAlSetenta/Problema63.java
@@ -0,0 +1,15 @@
+package ProblemasJava.SesentaiunoAlSetenta;
+
+public class Problema63 {
+
+    public static void main(String[] args) {
+
+        /*Dado dos números, diga si son amigos o no. Recuerde que dos números son amigos si la
+        suma de los divisores de uno de ellos es igual al otro y viceversa; por ejemplo, 220 y 284 son amigos:
+        Divisores de 220 son: 1 + 2 + 4 +S + 10 + 11 + 20 + 22 + 44 +55+ 110 = 284
+        Divisores de 284 son: 1 + 2 + 4 + 71 + 142 = 220*/
+
+        //Variables
+
+    }
+}
