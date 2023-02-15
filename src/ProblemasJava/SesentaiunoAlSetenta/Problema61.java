ce9271e79153c9aa76a52a3242291908b13f5377
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-14 23:40:26 -0300

    Problema 61.

---
+++ src/ProblemasJava/SesentaiunoAlSetenta/Problema61.java
@@ -0,0 +1,19 @@
+package ProblemasJava.SesentaiunoAlSetenta;
+
+import java.util.Scanner;
+
+public class Problema61 {
+
+    public static void main (String args){
+
+        //Enunciado: Hallar el cuadrado de un número usando la siguiente relación N2 = 1 + 3 + 5 + ... .. + 2N -1.
+
+        //Variables
+        int iterador, numero, cuadrado = 0;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese Número");
+
+    }
+}
