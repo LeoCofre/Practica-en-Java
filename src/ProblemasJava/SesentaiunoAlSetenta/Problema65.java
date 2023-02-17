a3431ee88dfc4226d1f678bf0d1d80e4b01aca2b
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-16 22:42:51 -0300

    Problema 65.

---
+++ src/ProblemasJava/SesentaiunoAlSetenta/Problema65.java
@@ -0,0 +1,20 @@
+package ProblemasJava.SesentaiunoAlSetenta;
+
+import java.util.Scanner;
+
+public class Problema65 {
+
+    public static void main(String[] args) {
+
+        /*Dado un rango numérico entero (número inicial y número final), obtener la cantidad de
+        números positivos y negativos que existen en el rango.*/
+
+        //variables
+        int numeroInicial, numeroFinal, cantidadPositivos = 0, cantidadNegativos = 0, iterador;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+
+
+    }
+}
