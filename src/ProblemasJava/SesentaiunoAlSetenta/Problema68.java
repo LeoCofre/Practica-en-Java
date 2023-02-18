af2d739169da52966d2f57048a27ed99ee90b208
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-17 23:56:07 -0300

    Problema 68.

--- src/ProblemasJava/SesentaiunoAlSetenta/Problema68.java
+++ src/ProblemasJava/SesentaiunoAlSetenta/Problema68.java
@@ -1,9 +1,24 @@
 package ProblemasJava.SesentaiunoAlSetenta;
 
+import java.util.Scanner;
+
 public class Problema68 {
 
     public static void main(String[] args) {
 
-        //
+        //Determine si un número es primo. Se dice que un número es primo si es divisible entre 1 y entre sí mismo.
+
+        //Variables
+        int numero, iterador;
+        boolean flag;
+        String respuesta;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese número: ");
+        numero = teclado.nextInt();
+
+
+
     }
 }
