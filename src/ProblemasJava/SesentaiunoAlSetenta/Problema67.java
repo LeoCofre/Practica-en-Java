d202119fde7b4734f14b6d47e25d055c77f1ae7c
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-17 23:45:53 -0300

    Problema 67.

--- src/ProblemasJava/SesentaiunoAlSetenta/Problema67.java
+++ src/ProblemasJava/SesentaiunoAlSetenta/Problema67.java
@@ -1,4 +1,30 @@
 package ProblemasJava.SesentaiunoAlSetenta;
 
+import java.util.Scanner;
+
 public class Problema67 {
+
+    public static void main(String[] args) {
+
+        /*Crear un algoritmo para hallar el factorial de un número. El factorial es el producto de todos
+        los números consecutivos, desde la unidad hasta el número; por ejemplo, factorial de 3! (se denota !) es
+        1 X 2 X 3 = 6. */
+
+        //Variables
+        int numero, factorial, iterador;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese número: ");
+        numero = teclado.nextInt();
+
+        //Proceso
+        factorial = 1;
+        for (iterador = 1; iterador <= numero; iterador++)
+            factorial *= iterador;
+
+        //Salida
+        System.out.println();
+        System.out.println("El factorial es: " + factorial);
+    }
 }
