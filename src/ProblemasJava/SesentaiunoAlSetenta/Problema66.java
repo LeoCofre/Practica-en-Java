9333b5202f1d401d6cd137f1b5e7bcd0f4e67835
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-17 23:17:56 -0300

    Problema 66.

--- src/ProblemasJava/SesentaiunoAlSetenta/Problema66.java
+++ src/ProblemasJava/SesentaiunoAlSetenta/Problema66.java
@@ -4,5 +4,10 @@ public class Problema66 {
 
     public static void main(String[] args) {
 
+        //Hallar cuántos múltiplos de M hay en un rango de números enteros.
+
+        //Variables
+        int numeroInicial, numeroFinal, numeroMultiplo, c = 0, iterador;
+
     }
 }
