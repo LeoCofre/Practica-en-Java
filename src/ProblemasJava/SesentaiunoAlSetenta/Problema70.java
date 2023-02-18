dc783cb461bc22c54dd0ca7aee2957909f6f54c1
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-18 00:25:40 -0300

    Problema 70.

---
+++ src/ProblemasJava/SesentaiunoAlSetenta/Problema70.java
@@ -0,0 +1,12 @@
+package ProblemasJava.SesentaiunoAlSetenta;
+
+public class Problema70 {
+
+    public static void main(String[] args) {
+
+        /*Dado un número y un divisor, determine cuál es el número múltiplo antecesor al número
+        ingresado; por ejemplo, si ingresa N = 21 y D = 3, entonces R = 18 porque es el número múltiplo de 3
+        antecesor de 21. */
+    }
+
+}
