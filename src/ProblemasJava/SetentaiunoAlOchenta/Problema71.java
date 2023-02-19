8a0977f302618bfa47c696c42bf34c0808a94a57
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-19 00:16:00 -0300

    Problema 71.

--- src/ProblemasJava/SetentaiunoAlOchenta/Problema71.java
+++ src/ProblemasJava/SetentaiunoAlOchenta/Problema71.java
@@ -1,10 +1,32 @@
 package ProblemasJava.SetentaiunoAlOchenta;
 
+import java.util.Scanner;
+
 public class Problema71 {
 
     public static void main(String[] args) {
 
         //Dado S números obtener la suma.
 
+        //Variables
+        int iterador, suma = 0;
+
+        //Arreglos
+        int numeros [] = new int[5];
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese primer número: ");
+        numeros[0]= teclado.nextInt();
+        System.out.println("Ingrese segundo número: ");
+        numeros[1]= teclado.nextInt();
+        System.out.println("Ingrese tercer número: ");
+        numeros[2]= teclado.nextInt();
+        System.out.println("Ingrese cuarto número: ");
+        numeros[3]= teclado.nextInt();
+        System.out.println("Ingrese quinto número: ");
+        numeros[4]= teclado.nextInt();
+
+        //Proceso
     }
 }
