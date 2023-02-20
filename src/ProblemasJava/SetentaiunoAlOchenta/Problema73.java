60f02f8d95cf473f9b5598b020b146affd45ce9b
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-20 16:29:50 -0300

    Problema 73.

--- src/ProblemasJava/SetentaiunoAlOchenta/Problema73.java
+++ src/ProblemasJava/SetentaiunoAlOchenta/Problema73.java
@@ -1,9 +1,34 @@
 package ProblemasJava.SetentaiunoAlOchenta;
 
+import java.util.Scanner;
+
 public class Problema73 {
 
     public static void main(String[] args) {
 
+        /*Dado 5 números y un divisor, determinar cuantos números múltiplos hay del divisor en los 5
+        números ingresados.*/
+
+        //Variables
+        int divisor, iterador, cantidad = 0;
+
+        //Arreglos
+        int[] numeros = new int[5];
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese primer número:");
+        numeros[0] = teclado.nextInt();
+        System.out.println("Ingrese segundo número:");
+        numeros[1] = teclado.nextInt();
+        System.out.println("Ingrese tercer número:");
+        numeros[2] = teclado.nextInt();
+        System.out.println("Ingrese cuarto número:");
+        numeros[3] = teclado.nextInt();
+        System.out.println("Ingrese quinto número:");
+        numeros[4] = teclado.nextInt();
+        System.out.println("Ingrese divisor:");
+        divisor = teclado.nextInt();
     }
 
 }
