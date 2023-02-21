bf39368bc19ff925ed6f45098c5b2e11f5ba2d09
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-20 22:15:56 -0300

    Problema 74.

--- src/ProblemasJava/SetentaiunoAlOchenta/Problema74.java
+++ src/ProblemasJava/SetentaiunoAlOchenta/Problema74.java
@@ -2,4 +2,14 @@ package ProblemasJava.SetentaiunoAlOchenta;
 
 public class Problema74 {
 
+    public static void main(String[] args) {
+
+        //Dado 5 números, obtener la cantidad de números primos ingresados.
+
+        //Variables
+        int cantidadPrimos = 0, i, j;
+
+
+
+    }
 }
