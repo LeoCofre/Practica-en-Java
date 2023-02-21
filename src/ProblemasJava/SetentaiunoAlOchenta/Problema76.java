dee3e6d69d0d98edfe468743764654392c345564
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-20 23:43:28 -0300

    Problema 76.

---
+++ src/ProblemasJava/SetentaiunoAlOchenta/Problema76.java
@@ -0,0 +1,12 @@
+package ProblemasJava.SetentaiunoAlOchenta;
+
+public class Problema76 {
+
+    public static void main(String[] args) {
+
+        /*Lea 4 números y almacénelos en un vector de llamado A, y otros 4 números en un vector
+        llamado B, y determine cuántos números de A se encuentran en B.*/
+
+        //Variables
+    }
+}
