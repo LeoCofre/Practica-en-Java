14caa402a442a865ca934d4d4312cfc7f622efc1
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-20 22:21:17 -0300

    Problema 74.

--- src/ProblemasJava/SetentaiunoAlOchenta/Problema74.java
+++ src/ProblemasJava/SetentaiunoAlOchenta/Problema74.java
@@ -1,5 +1,7 @@
 package ProblemasJava.SetentaiunoAlOchenta;
 
+import java.util.Scanner;
+
 public class Problema74 {
 
     public static void main(String[] args) {
@@ -8,7 +10,23 @@ public class Problema74 {
 
         //Variables
         int cantidadPrimos = 0, i, j;
+        boolean flag;
 
+        //Arreglos
+        int[] numeros = new int[5];
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese primer número");
+        numeros[0] = scanner.nextInt();
+        System.out.println("Ingrese segundo número");
+        numeros[1] = scanner.nextInt();
+        System.out.println("Ingrese tercer número");
+        numeros[2] = scanner.nextInt();
+        System.out.println("Ingrese cuarto número");
+        numeros[3] = scanner.nextInt();
+        System.out.println("Ingrese quinto número");
+        numeros[4] = scanner.nextInt();
 
 
     }
