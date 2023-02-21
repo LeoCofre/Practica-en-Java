83a3ba24d529b959ecf0e6e02873c7e3f3053a2f
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-20 23:51:28 -0300

    Problema 76.

--- src/ProblemasJava/SetentaiunoAlOchenta/Problema76.java
+++ src/ProblemasJava/SetentaiunoAlOchenta/Problema76.java
@@ -1,5 +1,7 @@
 package ProblemasJava.SetentaiunoAlOchenta;
 
+import java.util.Scanner;
+
 public class Problema76 {
 
     public static void main(String[] args) {
@@ -13,5 +15,18 @@ public class Problema76 {
         //Arreglos
         int[] a = new int[4];
         int[] b = new int[4];
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Arreglo A: ");
+        System.out.println("==========");
+        System.out.print("Numero 1: ");
+        a[0] = teclado.nextInt();
+        System.out.print("Numero 2: ");
+        a[1] = teclado.nextInt();
+        System.out.print("Numero 3: ");
+        a[2] = teclado.nextInt();
+        System.out.print("Numero 4: ");
+        a[3] = teclado.nextInt();
     }
 }
