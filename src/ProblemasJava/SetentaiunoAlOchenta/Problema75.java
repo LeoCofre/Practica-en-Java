32ac66e309524ba298cb2e6b0ff50f8c58169ea5
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-20 23:03:48 -0300

    Problema 75.

---
+++ src/ProblemasJava/SetentaiunoAlOchenta/Problema75.java
@@ -0,0 +1,13 @@
+package ProblemasJava.SetentaiunoAlOchenta;
+
+public class Problema75 {
+
+    public static void main(String[] args) {
+
+        /*Busque un número en 7 números ingresados y determine la posición y si existe o no el
+        número buscado, use el método de búsqueda secuencial.*/
+
+        //Variables
+
+    }
+}
