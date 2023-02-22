e9d116dc41e4d88a231eabbf107bca780c5867af
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-21 23:59:29 -0300

    Problema 79.

---
+++ src/ProblemasJava/SetentaiunoAlOchenta/Problema79.java
@@ -0,0 +1,12 @@
+package ProblemasJava.SetentaiunoAlOchenta;
+
+public class Problema79 {
+
+    public static void main(String[] args) {
+
+        /*Ingrese 12 números en un arreglo bidimensional (matriz) de 4x3, y obtenga la suma de cada
+        columna.*/
+
+
+    }
+}
