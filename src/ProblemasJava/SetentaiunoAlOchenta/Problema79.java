06e825c3f50c429ee5491bcf4b0dfa238aab8d3b
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-22 00:24:16 -0300

    Problema 79.

--- src/ProblemasJava/SetentaiunoAlOchenta/Problema79.java
+++ src/ProblemasJava/SetentaiunoAlOchenta/Problema79.java
@@ -1,11 +1,46 @@
 package ProblemasJava.SetentaiunoAlOchenta;
 
+import java.util.Scanner;
+
 public class Problema79 {
 
     public static void main(String[] args) {
 
         /*Ingrese 12 números en un arreglo bidimensional (matriz) de 4x3, y obtenga la suma de cada
         columna.*/
+
+        //Variables
+        int sumaColumnaCero = 0, sumaColumnaUno = 0, sumaColumnaDos = 0, i, j;
+
+        //Arreglos
+        int[][] numeros = new int[4][3];
+
+        //Entrada
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Número (0,0)");
+        numeros[0][0] = scanner.nextInt();
+        System.out.println("Número (0,1)");
+        numeros[0][1] = scanner.nextInt();
+        System.out.println("Número (0,2)");
+        numeros[0][2] = scanner.nextInt();
+        System.out.println("Número (1,0)");
+        numeros[1][0] = scanner.nextInt();
+        System.out.println("Número (1,1)");
+        numeros[1][1] = scanner.nextInt();
+        System.out.println("Número (1,2)");
+        numeros[1][2] = scanner.nextInt();
+        System.out.println("Número (2,0)");
+        numeros[2][0] = scanner.nextInt();
+        System.out.println("Número (2,1)");
+        numeros[2][1] = scanner.nextInt();
+        System.out.println("Número (2,2)");
+        numeros[2][2] = scanner.nextInt();
+        System.out.println("Número (3,0)");
+        numeros[3][0] = scanner.nextInt();
+        System.out.println("Número (3,1)");
+        numeros[3][1] = scanner.nextInt();
+        System.out.println("Número (3,2)");
+        numeros[3][2] = scanner.nextInt();
 
 
     }
