a1b59697f271f1813e84120fa1db0a895fc268f5
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-21 23:31:24 -0300

    Problema 77.

--- src/ProblemasJava/SetentaiunoAlOchenta/Problema77.java
+++ src/ProblemasJava/SetentaiunoAlOchenta/Problema77.java
@@ -4,5 +4,13 @@ public class Problema77 {
 
     public static void main(String[] args) {
 
+        //Ordene 4 números usando el método de ordenación por intercambio (burbuja).
+
+        //Variables
+        int tmp, i, j, LI, LS;
+
+        //Arreglos
+        int[] n = new int[4];
+
     }
 }
