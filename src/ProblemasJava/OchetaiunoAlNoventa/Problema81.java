31127562b95225e63eb90289efc34816c65923ac
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-23 22:54:36 -0300

    Problema 81.

---
+++ src/ProblemasJava/OchetaiunoAlNoventa/Problema81.java
@@ -0,0 +1,18 @@
+package ProblemasJava.OchetaiunoAlNoventa;
+
+public class Problema81 {
+
+    public static void main(String[] args) {
+
+        /*Busque un número dentro de una matriz de 4x3, determine la posición y si existe o no el
+        número buscado. Use el método de búsqueda secuencial.*/
+
+        //Variables
+        int nb, i, j, pl, p2;
+        String r;
+
+        //Arreglos
+        int[][] numeros = new int[4][3];
+
+    }
+}
