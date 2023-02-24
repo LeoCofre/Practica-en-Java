b96debd7cd50ab4ef6ba42fe67888b9c03637c9c
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-23 23:58:01 -0300

    Problema 82.

---
+++ src/ProblemasJava/OchetaiunoAlNoventa/Problema83.java
@@ -0,0 +1,12 @@
+package ProblemasJava.OchetaiunoAlNoventa;
+
+public class Problema83 {
+
+    public static void main(String[] args) {
+
+        //Ingrese 6 números en una matriz de 3x2 y obtenga el numero mayor ingresado.
+
+        //Variables
+
+    }
+}
