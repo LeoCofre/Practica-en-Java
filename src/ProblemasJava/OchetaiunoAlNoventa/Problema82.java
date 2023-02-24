2be628d74a7a33b8e5a77a18e25640742f4d6bca
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-23 23:19:55 -0300

    Problema 81.

---
+++ src/ProblemasJava/OchetaiunoAlNoventa/Problema82.java
@@ -0,0 +1,19 @@
+package ProblemasJava.OchetaiunoAlNoventa;
+
+public class Problema82 {
+
+    public static void main(String[] args) {
+
+        //Dado la matriz A de 2x2, la matriz B de 2x2, obtenga la suma de dichas matriz.
+
+        //Variables
+        int i, j;
+
+        //Arreglos
+        int a[][] = new int[2][2];
+        int b[][] = new int[2][2];
+        int c[][] = new int[2][2];
+
+        //Entrada
+    }
+}
