47af8222d188cef211001d18f5043c1f5b01215c
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-23 23:09:04 -0300

    Problema 81.

--- src/ProblemasJava/OchetaiunoAlNoventa/Problema81.java
+++ src/ProblemasJava/OchetaiunoAlNoventa/Problema81.java
@@ -1,5 +1,7 @@
 package ProblemasJava.OchetaiunoAlNoventa;
 
+import java.util.Scanner;
+
 public class Problema81 {
 
     public static void main(String[] args) {
@@ -14,5 +16,33 @@ public class Problema81 {
         //Arreglos
         int[][] numeros = new int[4][3];
 
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Numero (0,0): ");
+        numeros[0][0] = teclado.nextInt();
+        System.out.print("Numero (0,1): ");
+        numeros[0][1] = teclado.nextInt();
+        System.out.print("Numero (0,2): ");
+        numeros[0][2] = teclado.nextInt();
+        System.out.print("Numero (1,0): ");
+        numeros[1][0] = teclado.nextInt();
+        System.out.print("Numero (1,1): ");
+        numeros[1][1] = teclado.nextInt();
+        System.out.print("Numero (1,2): ");
+        numeros[1][2] = teclado.nextInt();
+        System.out.print("Numero (2,0): ");
+        numeros[2][0] = teclado.nextInt();
+        System.out.print("Numero (2,1): ");
+        numeros[2][1] = teclado.nextInt();
+        System.out.print("Numero (2,2): ");
+        numeros[2][2] = teclado.nextInt();
+        System.out.print("Numero (3,0): ");
+        numeros[3][0] = teclado.nextInt();
+        System.out.print("Numero (3,1): ");
+        numeros[3][1] = teclado.nextInt();
+        System.out.print("Numero (3,2): ");
+        numeros[3][2] = teclado.nextInt();
+        System.out.println("Numero a buscar");
+        nb = teclado.nextInt();
     }
 }
