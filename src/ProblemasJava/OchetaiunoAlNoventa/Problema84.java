b2ea8bb7887a0ff25348baa63d680bd7372b7c5b
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-24 23:29:40 -0300

    Problema 84.

---
+++ src/ProblemasJava/OchetaiunoAlNoventa/Problema84.java
@@ -0,0 +1,11 @@
+package ProblemasJava.OchetaiunoAlNoventa;
+
+public class Problema84 {
+
+    public static void main(String[] args) {
+
+        //Ingrese 6 números en una matriz de 3x2 y ordene los números de cada columna.
+
+
+    }
+}
