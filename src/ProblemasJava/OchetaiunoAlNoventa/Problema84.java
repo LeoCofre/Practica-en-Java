b2eb34a994fa3ef195a46637f75a3038ff966492
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-24 23:45:41 -0300

    Problema 84.

--- src/ProblemasJava/OchetaiunoAlNoventa/Problema84.java
+++ src/ProblemasJava/OchetaiunoAlNoventa/Problema84.java
@@ -1,5 +1,7 @@
 package ProblemasJava.OchetaiunoAlNoventa;
 
+import java.util.Scanner;
+
 public class Problema84 {
 
     public static void main(String[] args) {
@@ -11,5 +13,20 @@ public class Problema84 {
 
         //Arreglos
         int[][] n = new int[3][2];
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print(" Numero (0 , O) : ");
+        n[0][0] = teclado.nextInt();
+        System.out.print(" Numero (0 , 1) : ");
+        n[1][1] = teclado.nextInt();
+        System.out.print(" Numero (1, 0 ): ");
+        n[1][0] = teclado.nextInt();
+        System.out.print(" Numero (1 , 1 ): ");
+        n[1][1] = teclado.nextInt();
+        System.out.print(" Numero (2 , 0) : ");
+        n[2][0] = teclado.nextInt();
+        System.out.print (" Numero (2 , 1 ): ");
+        n[2][1] = teclado.nextInt();
     }
 }
