7905c26a6bd93e6c2a3725384047d9a83545b74d
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-25 19:05:44 -0300

    Problema 85.

--- src/ProblemasJava/OchetaiunoAlNoventa/Problema85.java
+++ src/ProblemasJava/OchetaiunoAlNoventa/Problema85.java
@@ -4,5 +4,14 @@ public class Problema85 {
 
     public static void main(String[] args) {
 
+        //Almacene 9 números en una matriz de 3x3 y obtenga los números ordenados.
+
+        //Variables
+        int tmp, i, j, x, LS;
+
+        //Arreglos
+        int[][] n =new int [3] [3];
+        int[] o = new int[9];
+
     }
 }
