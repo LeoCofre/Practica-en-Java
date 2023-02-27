f9a64ed85dcd5a3afddcc4291c719f21f0159d35
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-27 00:18:38 -0300

    Problema 86.

--- src/ProblemasJava/OchetaiunoAlNoventa/Problema86.java
+++ src/ProblemasJava/OchetaiunoAlNoventa/Problema86.java
@@ -1,8 +1,29 @@
 package ProblemasJava.OchetaiunoAlNoventa;
 
+import java.util.Scanner;
+
 public class Problema86 {
 
     public static void main(String[] args) {
+
+        // Dado un nombre, obtener la cantidad de caracteres que contiene.
+
+        //Variables
+        String nombre;
+        int cantidad;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese nombre: ");
+        nombre = teclado.nextLine();
+
+        //Proceso
+        cantidad = nombre.length();
+
+        //Salida
+        System.out.println();
+        System.out.println("La cantidad de letras que contiene el nombre es: " + cantidad);
+
 
     }
 }
