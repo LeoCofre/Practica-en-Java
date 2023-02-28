02eb6d3f55554bc1d6489b38424efe48e9290bc4
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-28 00:10:50 -0300

    Problema 90.

--- src/ProblemasJava/OchetaiunoAlNoventa/Problema90.java
+++ src/ProblemasJava/OchetaiunoAlNoventa/Problema90.java
@@ -1,9 +1,33 @@
 package ProblemasJava.OchetaiunoAlNoventa;
 
+import java.util.Scanner;
+
 public class Problema90 {
 
     public static void main(String[] args) {
 
+        //Dado un carácter, determine si es una letra, número o símbolo.
 
+        //Variables
+        String c, r;
+        int a;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Caracter : ");
+        c = teclado.nextLine();
+
+        //Proceso
+        a = c.toUpperCase().codePointAt(0);
+        if (a >= 48 && a <= 57)
+            r = " ES NUMERO";
+        else if (a >= 65 && a <= 90)
+            r = "ES LETRA";
+        else
+            r = "ES SIMBOLO";
+
+        //Salida
+        System.out.println();
+        System.out.println("El caracter ingresado: " + r);
     }
 }
