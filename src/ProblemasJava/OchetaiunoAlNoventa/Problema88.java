66164e4615c7b28909b32282f774c8b9e637de2a
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-27 23:41:29 -0300

    Problema 87.

---
+++ src/ProblemasJava/OchetaiunoAlNoventa/Problema88.java
@@ -0,0 +1,9 @@
+package ProblemasJava.OchetaiunoAlNoventa;
+
+public class Problema88 {
+
+    public static void main(String[] args) {
+
+        //Dado un carácter, devolver su código ASCII.
+    }
+}
