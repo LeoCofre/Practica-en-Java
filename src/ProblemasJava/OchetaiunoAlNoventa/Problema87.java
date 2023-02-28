69ef39c1e70d835cffaac95a2f2f50910e3738da
Leonardo Cofré <leocofre1981@gmail.com>
2023-02-27 23:33:01 -0300

    Problema 87.

---
+++ src/ProblemasJava/OchetaiunoAlNoventa/Problema87.java
@@ -0,0 +1,16 @@
+package ProblemasJava.OchetaiunoAlNoventa;
+
+public class Problema87 {
+
+    public static void main(String[] args) {
+
+        /*Ingrese su nombre y apellido, y obtenga su nombre y apellido en mayúscula separado por
+        una coma XXXXX, XXXXX. */
+
+        //Variables
+        String nombre, apellido, nombreApellidoMayuscula;
+
+        //Entrada
+
+    }
+}
