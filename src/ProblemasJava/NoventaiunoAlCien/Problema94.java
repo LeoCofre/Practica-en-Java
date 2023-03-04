ac362700aa91fcd37a22b6e633b936d51b664014
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-03 23:44:34 -0300

    Problema 93.

---
+++ src/ProblemasJava/NoventaiunoAlCien/Problema94.java
@@ -0,0 +1,9 @@
+package ProblemasJava.NoventaiunoAlCien;
+
+public class Problema94 {
+
+    public static void main(String[] args) {
+
+        //Dado una frase devolver la frase sin espacio en blancos.
+    }
+}
