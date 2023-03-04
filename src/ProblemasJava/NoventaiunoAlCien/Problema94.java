1b0e472af875f52da8c3179104cc1a659e6ad38a
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-03 23:56:04 -0300

    Problema 93.

--- src/ProblemasJava/NoventaiunoAlCien/Problema94.java
+++ src/ProblemasJava/NoventaiunoAlCien/Problema94.java
@@ -1,9 +1,23 @@
 package ProblemasJava.NoventaiunoAlCien;
 
+import java.util.Scanner;
+
 public class Problema94 {
 
     public static void main(String[] args) {
 
         //Dado una frase devolver la frase sin espacio en blancos.
+
+
+        //Variables
+        String fl, f2 = "", t;
+        int i, p;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Frase : ");
+        fl = teclado.nextLine();
+
+
     }
 }
