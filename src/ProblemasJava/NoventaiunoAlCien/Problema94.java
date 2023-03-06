f4f562aab324e65774917b1d134e34be0f01b6e2
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-05 23:40:24 -0300

    Problema 94.

--- src/ProblemasJava/NoventaiunoAlCien/Problema94.java
+++ src/ProblemasJava/NoventaiunoAlCien/Problema94.java
@@ -37,28 +37,30 @@ public class Problema94 {
     public static void main(String[] args) {
 
         //Variables
-        String fl, f2 = "", t;
-        int i, p;
+        String frase, fraseSinEspacios = "", acumuladorTemporal;
+        int iterador, posicion;
 
         //Entrada
         Scanner teclado = new Scanner(System.in);
         System.out.print("Frase : ");
-        fl = teclado.nextLine();
+        frase = teclado.nextLine();
 
         //Proceso
-        p = 0;
-        fl = fl.trim();
-        for (i = 0; i < fl.length(); i++) {
-            if (fl.substring(i, i + 1).equals(" ")) {
-                t = fl.substring(p, i);
-                p = i + 1;
+        posicion = 0;
+        frase = frase.trim();
+        for (iterador = 0; iterador < frase.length(); iterador++) {
+            if (frase.substring(iterador, iterador + 1).equals(" ")) {
+                acumuladorTemporal = frase.substring(posicion, iterador);
+                posicion = iterador + 1;
+                fraseSinEspacios = fraseSinEspacios + acumuladorTemporal;
+
             }
         }
-        t = fl.substring(p, i);
-        f2 = f2 + t;
+        acumuladorTemporal = frase.substring(posicion, iterador);
+        fraseSinEspacios = fraseSinEspacios + acumuladorTemporal;
 
         // Salida
         System.out.println("");
-        System.out.println(" Frase sin espacios: " + f2);
+        System.out.println(" Frase sin espacios: " + fraseSinEspacios);
     }
 }
