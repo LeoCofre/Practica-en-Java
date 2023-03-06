87759bda0b467146ff28ff062c63ebbdd44f81cb
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-05 23:41:34 -0300

    Problema 94.

--- src/ProblemasJava/NoventaiunoAlCien/Problema94.java
+++ src/ProblemasJava/NoventaiunoAlCien/Problema94.java
@@ -4,37 +4,10 @@ import java.util.Scanner;
 
 public class Problema94 {
 
-
-    //Dado una frase devolver la frase sin espacio en blancos.
-/*
-        //Variables
-        String frase;
-        StringBuilder fraseSinEspacios = new StringBuilder();
-        String temporal;
-        int iterador, posicion;
-
-        //Entrada
-        Scanner teclado = new Scanner(System.in);
-        System.out.print("Frase : ");
-        frase = teclado.nextLine();
-
-        //Proceso
-        posicion = 0;
-        frase = frase.trim();
-        for (iterador = 0; iterador < frase.length(); iterador++)
-            if (frase.charAt(iterador) == ' ') {
-                temporal = frase.substring(posicion, iterador);
-                posicion = iterador + 1;
-                fraseSinEspacios.append(temporal);
-                temporal = frase.substring(posicion, iterador);
-                fraseSinEspacios.append(temporal);
-            }
-        //Salida
-        System.out.println();
-        System.out.println("Frase sin espacios: " + fraseSinEspacios);
-    }*/
-
     public static void main(String[] args) {
+
+        //Dado una frase devolver la frase sin espacio en blancos.
+
 
         //Variables
         String frase, fraseSinEspacios = "", acumuladorTemporal;
