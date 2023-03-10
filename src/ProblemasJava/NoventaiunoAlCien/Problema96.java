295c28ed73c83055189ee0dd587d8c594eedb1b9
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-10 19:59:09 -0300

    Problema 95.

---
+++ src/ProblemasJava/NoventaiunoAlCien/Problema96.java
@@ -0,0 +1,12 @@
+package ProblemasJava.NoventaiunoAlCien;
+
+public class Problema96 {
+
+    public static void main(String[] args) {
+
+        //Dados dos números enteros, hallar la suma. Cree una función para resolver el problema.
+
+        //Variables
+
+    }
+}
