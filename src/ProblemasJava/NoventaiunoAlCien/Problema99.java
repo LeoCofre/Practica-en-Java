ef96d579512a7fa3586f368259045514b901fbfc
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-10 20:30:56 -0300

    Problema 97.

---
+++ src/ProblemasJava/NoventaiunoAlCien/Problema99.java
@@ -0,0 +1,43 @@
+package ProblemasJava.NoventaiunoAlCien;
+
+import java.util.Scanner;
+
+public class Problema99 {
+
+    public static void main(String[] args) {
+
+        /*Dado un número, determinar cuántos dígitos tiene. Cree una función para resolver el
+        problema.*/
+
+        //Variables
+        int n, e = 0;
+
+        //Entrada
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Numero");
+        n = teclado.nextInt();
+
+        //Proceso
+        e = CantidadDigitos(n);
+
+        //Salida
+        System.out.println("");
+        System.out.println("Cant . Digitos : " + e);
+    }
+
+    //Método Cantidad Digitos
+    private static int CantidadDigitos(int num) {
+
+        //Variables
+        int c = 0;
+
+        //Proceso
+        while (num > 0) {
+            num = num / 10;
+            c += 1;
+        }
+
+        //Salida
+        return c;
+    }
+}
