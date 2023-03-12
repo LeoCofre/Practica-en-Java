19a56fc724cdbe718d279bc713d21056b1cfda71
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-11 22:52:16 -0300

    Problema103

--- src/ProblemasJava/CientounoAlCientocinco/Problema103.java
+++ src/ProblemasJava/CientounoAlCientocinco/Problema103.java
@@ -31,6 +31,33 @@ public class Problema103 {
         System.out.print(" Numero (2 , 0) : ");
         n[2][0] = teclado.nextInt();
         System.out.print(" Numero (2 , 1 ) : ");
-        n[2] [1] =teclado.nextInt();
+        n[2][1] = teclado.nextInt();
+
+        //Proceso
+        m = Mayor(n);
+
+        //Salida
+        System.out.println("");
+        System.out.println("Mayor : " + m);
+    }
+
+    //Método Mayor
+    private static int Mayor(int num[][]) {
+
+        //Variables
+        int m, i, j;
+
+        //Proceso
+        m = num[0][0];
+        for (i = 0; i <= 2; i++) {
+            for (j = 0; j <= 1; j++) {
+                if (num[i][j] > m)
+                    m = num[i][j];
+            }
+        }
+        //Salida
+        return m;
     }
 }
+
+
