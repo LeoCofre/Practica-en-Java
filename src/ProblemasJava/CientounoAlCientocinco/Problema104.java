60211a4076c8a480d5a89f266043af48b53002df
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-11 23:20:18 -0300

    Problema103

--- src/ProblemasJava/CientounoAlCientocinco/Problema104.java
+++ src/ProblemasJava/CientounoAlCientocinco/Problema104.java
@@ -15,7 +15,7 @@ public class Problema104 {
         //Arreglos
         int[][] a = new int[2][2];
         int[][] b = new int[2][2];
-        int[][] e = new int[2][2];
+        int[][] c = new int[2][2];
 
         //Entrada
         Scanner teclado = new Scanner(System.in);
@@ -30,7 +30,7 @@ public class Problema104 {
         System.out.print(" Numero ( 1 , 1) : ");
         a[1][1] = teclado.nextInt();
         System.out.println();
-        System.out.println("Arreglo B: ");
+        9System.ou't.println("Arreglo B: ");
         System.out.println(" =========== : ");
         System.out.print(" Numero (0 , 0) : ");
         b[0][0] = teclado.nextInt();
@@ -41,5 +41,34 @@ public class Problema104 {
         System.out.print(" Numero (1 , 1 ) : ");
         b[1][1] = teclado.nextInt();
 
+        //Proceso
+         c = SumaArreglos(a, b);
+        // Salida
+        System.out.println("");
+        System.out.println("Arreglo C : ");
+        System.out.println(" =========== : ");
+        System.out.println(" Numero (0 , 0) : " + c[0][0]);
+        System.out.println(" Numero (0 , 1) : " + c[0][1]);
+        System.out.println(" Numero (1 , 0) : " + c[1][0]);
+        System.out.println(" Numero (1 , 1) : " + c[1][1]);
+
+    }
+
+    private static int[][] SumaArreglos(int ArrA[][], int ArrB[][]) {
+
+        //Variables
+        int i, j;
+
+        //Arreglos
+        int[][] s = new int[2][2];
+
+        //Proceso
+        for (i = 0; i <= 1; i++) {
+            for (j = 0; j <= 1; j++)
+                s[i][j] = ArrA[i][j] + ArrB[i][j];
+        }
+
+        //Salida
+        return s;
     }
 }
