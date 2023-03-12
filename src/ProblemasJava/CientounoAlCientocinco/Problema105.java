10ba84acac51a68478a199f734059469a0dee902
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-12 20:35:32 -0300

    Problema105

--- src/ProblemasJava/CientounoAlCientocinco/Problema105.java
+++ src/ProblemasJava/CientounoAlCientocinco/Problema105.java
@@ -1,4 +1,11 @@
 package ProblemasJava.CientounoAlCientocinco;
 
 public class Problema105 {
+
+    /*Dado una frase devolver la frase sin espacio en blancos. Cree una función para resolver el
+    problema.
+
+    FraseSinEspacios( E: Frase: Cadena) :Cadena
+    */
+
 }
