197eaea09bd51a268e512398364cbb01cdd2cd5e
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-11 22:43:22 -0300

    Problema102 .

---
+++ src/ProblemasJava/CientounoAlCientocinco/Problema103.java
@@ -0,0 +1,19 @@
+package ProblemasJava.CientounoAlCientocinco;
+
+public class Problema103 {
+
+    /*Ingrese 6 números en una matriz de 3x2 y obtenga el numero mayor ingresado. Cree una
+    función para resolver el problema.
+
+        Mayor{ E :num [] :Entero):Entero
+    */
+
+    public static void main(String[] args) {
+
+        //Variables
+        int m;
+
+        //Arreglos
+        int[][] n = new int[3][2];
+    }
+}
