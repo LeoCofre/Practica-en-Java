47bdb44166f5c26f4544f1603f7da1e14bc7a21a
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-13 22:02:31 -0300

    Problema105

--- src/ProblemasJava/CientounoAlCientocinco/Problema105.java
+++ src/ProblemasJava/CientounoAlCientocinco/Problema105.java
@@ -29,4 +29,28 @@ public class Problema105 {
         System.out.println("Frase sin espacios: " + f2);
     }
 
+    //Método Frase Sin Espacios
+    private static String FraseSinEspacios(String Frase) {
+
+        //Variables
+        String f = "", t;
+        int i, p;
+
+        //Proceso
+        p = 0;
+        Frase = Frase.trim();
+        for (i = 0; i < Frase.length(); i++) {
+
+            if (Frase.charAt(i) == ' ') {
+                t = Frase.substring(p, i);
+                p = i + 1;
+                f = f + t;
+            }
+        }
+        t = Frase.substring(p, i);
+        f = f + t;
+
+        //Salida
+        return f;
+    }
 }
