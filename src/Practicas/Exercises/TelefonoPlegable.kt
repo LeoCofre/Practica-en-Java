27b50fcc6cea48ac114c0e00ab7957a9caaa3cab
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-16 20:34:41 -0300

    ExerciseFive

--- src/Practicas/Exercises/TelefonoPlegable.kt
+++ src/Practicas/Exercises/TelefonoPlegable.kt
@@ -1,2 +1,14 @@
 package Practicas.Exercises
 
+/*
+Por lo general, la pantalla del teléfono se enciende y se apaga cuando se presiona el botón de encendido.
+En cambio, si un teléfono plegable está plegado, su pantalla interna principal no se enciende cuando
+se presiona el botón de encendido.
+
+En el código inicial que se proporciona en el siguiente fragmento de código, escribe una clase
+FoldablePhone que se herede de la clase Phone. Debe contener lo siguiente:
+
+-Una propiedad que indique si el teléfono está plegado
+-Un comportamiento de función switchOn() diferente del de la clase Phone para que solo encienda la pantalla cuando el teléfono no esté plegado
+-Métodos para cambiar el estado de plegado
+*/
