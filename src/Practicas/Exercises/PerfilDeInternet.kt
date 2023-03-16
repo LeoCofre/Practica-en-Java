d6bd909337bf94fba4cdf4721d5e651a5e0d9872
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-16 00:09:20 -0300

    ExerciseFive

--- src/Practicas/Exercises/PerfilDeInternet.kt
+++ src/Practicas/Exercises/PerfilDeInternet.kt
@@ -12,7 +12,7 @@ fun main() {
     val amanda = Person("Amanda", 33, "play tennis", null)
     val atiqah = Person("Atiqah", 28, "climb", amanda)
 
-    //amanda.showProfile()
+    amanda.showProfile()
     atiqah.showProfile()
 }
 
