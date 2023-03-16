2a9cd5c573c48dda8371ab926fadfc08718a75a9
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-15 23:59:54 -0300

    ExerciseFour

--- src/Practicas/Exercises/PerfilDeInternet.kt
+++ src/Practicas/Exercises/PerfilDeInternet.kt
@@ -7,3 +7,17 @@ que te refirieron para que registraras tu perfil.
 
 En el código inicial que se proporciona en el siguiente fragmento de código, escribe un programa que
 imprima los detalles del perfil de una persona.*/
+
+fun main() {
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+        // Fill in code
+    }
+}
