bf9ced62679fb9705dc79b2c3b922fbe050af20a
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-15 22:46:58 -0300

    ExerciseFour

---
+++ src/Practicas/Exercises/PerfilDeInternet.kt
@@ -0,0 +1,9 @@
+package Practicas.Exercises
+
+/*
+A menudo, debes completar los perfiles de sitios web en línea que contienen campos obligatorios y
+no obligatorios. Por ejemplo, puedes agregar tu información personal y un vínculo a otras personas
+que te refirieron para que registraras tu perfil.
+
+En el código inicial que se proporciona en el siguiente fragmento de código, escribe un programa que
+imprima los detalles del perfil de una persona.*/
