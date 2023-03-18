f0a9c6afdcc576538612f1a455b624edecf77508
Leonardo Cofré <leocofre1981@gmail.com>
2023-03-17 21:38:28 -0300

    ExerciseFive

--- src/Practicas/Exercises/TelefonoPlegable.kt
+++ src/Practicas/Exercises/TelefonoPlegable.kt
@@ -9,11 +9,13 @@ En el código inicial que se proporciona en el siguiente fragmento de código, e
 FoldablePhone que se herede de la clase Phone. Debe contener lo siguiente:
 
 -Una propiedad que indique si el teléfono está plegado
--Un comportamiento de función switchOn() diferente del de la clase Phone para que solo encienda la pantalla cuando el teléfono no esté plegado
+-Un comportamiento de función switchOn() diferente del de la clase Phone para que solo encienda la pantalla cuando
+ el teléfono no esté plegado
 -Métodos para cambiar el estado de plegado
 */
-class Phone(var isScreenLightOn: Boolean = false){
-    fun switchOn() {
+
+open class Phone(var isScreenLightOn: Boolean = false) {
+    open fun switchOn() {
         isScreenLightOn = true
     }
 
@@ -22,7 +24,33 @@ class Phone(var isScreenLightOn: Boolean = false){
     }
 
     fun checkPhoneScreenLight() {
-        val phoneScreenLight = if(isScreenLightOn) "on" else "off"
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
         println("The phone screen's light is $phoneScreenLight.")
     }
+}
+
+class FoldablePhone(var isFolded: Boolean = true) : Phone() {
+    override fun switchOn() {
+        if (!isFolded) {
+            isScreenLightOn = true
+        }
+    }
+
+    fun fold() {
+        isFolded = true
+    }
+
+    fun unfold() {
+        isFolded = false
+    }
+}
+
+fun main() {
+    val newFoldablePhone = FoldablePhone()
+
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+    newFoldablePhone.unfold()
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
 }
