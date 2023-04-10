1e12cbf468d8fabd5f0e875acd3490115f4ff2b9
Leonardo Cofré <leocofre1981@gmail.com>
2023-04-09 23:23:19 -0400

    El generador pseudoaleatorio

--- src/Practicas/Exercises/GeneradorPseudoaleatorio.kt
+++ src/Practicas/Exercises/GeneradorPseudoaleatorio.kt
@@ -1,2 +1,16 @@
 package Practicas.Exercises
 
+/*
+ * Crea un generador de números pseudoaleatorios entre 0 y 100.
+ * - No puedes usar ninguna función "random" (o semejante) del lenguaje de programación seleccionado.
+ *
+ * Es más complicado de lo que parece...
+ */
+
+fun main() {
+    (0..100).forEach { _ -> println(random()) }
+}
+
+private fun random(): Int {
+    return (System.nanoTime() % 101).toInt()
+}
