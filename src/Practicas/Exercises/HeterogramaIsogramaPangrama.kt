e3c21a107e2cdbc90481598cf6528068810650eb
Leonardo Cofré <leocofre1981@gmail.com>
2023-04-11 23:25:43 -0400

    Heterograma, isograma y pangrama

--- src/Practicas/Exercises/HeterogramaIsogramaPangrama.kt
+++ src/Practicas/Exercises/HeterogramaIsogramaPangrama.kt
@@ -1,2 +1,18 @@
 package Practicas.Exercises
 
+/*
+ * Crea 3 funciones, cada una encargada de detectar si una cadena de
+ * texto es un heterograma, un isograma o un pangrama.
+ * - Debes buscar la definición de cada uno de estos términos.
+ */
+
+
+fun heterograma(){
+
+}
+ fun isograma(){
+
+ }
+fun pangrama(){
+
+}
